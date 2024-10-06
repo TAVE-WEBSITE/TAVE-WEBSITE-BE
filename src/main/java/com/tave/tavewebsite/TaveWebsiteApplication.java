94d74b58b1e6440c2d450a7dbbc7bed3686db4fe
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-10-07 00:44:08 +0900

    init

---
+++ src/main/java/com/tave/tavewebsite/TaveWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.tave.tavewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaveWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaveWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tave/tavewebsite/TaveWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tave.tavewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaveWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
