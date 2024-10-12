c7eb1cee92479de9ab50401f184276bf50ea4984
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-10-12 23:48:35 +0900

    fix : BaseEntity 상속

--- src/main/java/com/tave/tavewebsite/domain/history/entity/History.java
+++ src/main/java/com/tave/tavewebsite/domain/history/entity/History.java
@@ -1,43 +1,41 @@
 package com.tave.tavewebsite.domain.history.entity;
 
+import com.tave.tavewebsite.global.common.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class History {
+public class History extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "historyId")
     private Long id;
 
+    @NotNull
     @Size(min = 1, max = 5)
     @Column(nullable = false)
     private String generation;
 
+    @NotNull
     @Size(min = 1, max = 500)
     @Column(nullable = false)
     private String description;
 
+    @NotNull
     @Column(nullable = false)
     private boolean isPublic;
 
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
-
+    @Builder
     public History(String generation, String description, boolean isPublic) {
         this.generation = generation;
         this.description = description;
