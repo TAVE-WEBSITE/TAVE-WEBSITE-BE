aea505a161280d01f028c3ecedf05480c2e7ff64
koreaioi <koreaioi@gachon.ac.kr>
2024-10-24 17:44:47 +0900

    feat: GlobalExceptionHandler 구현

---
+++ src/main/java/com/tave/tavewebsite/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.tave.tavewebsite.global.exception;
+
+
+import com.tave.tavewebsite.global.exception.Response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 로그 형식
+    private static final String LOG_FORMAT = "Class : {}, Code : {}, Message : {}";
+    private static final int ERROR_CODE = 400;
+    private static final int SERVER_ERROR_CODE = 500;
+
+    // 사용자 정의 예외 처리
+    @ExceptionHandler(BaseErrorException.class)
+    public <T> ApiResponse<T> handle(BaseErrorException e) {
+
+        logWarning(e, e.getErrorCode());
+
+        return ApiResponse.fail(e.getErrorCode(), e.getMessage());
+    }
+
+    // @Valid 예외 처리 (@NotNull, @Size, etc...) or IllegalArgumentException
+    @ExceptionHandler({MethodArgumentNotValidException.class, IllegalArgumentException.class})
+    public <T> ApiResponse<T> handle(MethodArgumentNotValidException e) {
+
+        logWarning(e, ERROR_CODE);
+
+        return ApiResponse.fail(ERROR_CODE, e.getMessage());
+    }
+
+
+    // 서버 측 에러 (이외의 에러)
+    @ExceptionHandler(Exception.class)
+    public <T> ApiResponse<T> handle(Exception e) {
+
+        logWarning(e, SERVER_ERROR_CODE);
+
+        return ApiResponse.fail(SERVER_ERROR_CODE, e.getMessage());
+    }
+
+    // log.warn이 중복되어 리팩토링
+    private void logWarning(Exception e, int errorCode) {
+        log.warn(e.getMessage(), e);  // 전체 로그 출력, 운영 단계에서는 삭제
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), errorCode, e.getMessage());
+    }
+}
