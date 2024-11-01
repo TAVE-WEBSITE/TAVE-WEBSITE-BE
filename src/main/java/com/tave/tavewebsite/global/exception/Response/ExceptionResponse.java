34911856eeff78704301bcce13786e7b10603de5
koreaioi <koreaioi@gachon.ac.kr>
2024-11-01 15:44:40 +0900

    remove: remove ExceptionResponse success method

--- src/main/java/com/tave/tavewebsite/global/exception/Response/ExceptionResponse.java
+++ src/main/java/com/tave/tavewebsite/global/exception/Response/ExceptionResponse.java
@@ -26,14 +26,4 @@ public class ExceptionResponse<T> {
 
         return new ExceptionResponse<>(code, message);
     }
-
-    /*
-    * 응답 성공과 실패 형식을 통일 한다면 아래 메소드 사용
-    * 1. code와 message는 응답 성공시 enum을 커스텀하여 넘겨준다.
-    * 2. T data는 Dto를 넘겨주면 됩니다.
-    * */
-//    public static <T> ApiResponse<T> success(int code, String message, T data) {
-//        return new ApiResponse<>(code, message, data);
-//    }
-
 }
