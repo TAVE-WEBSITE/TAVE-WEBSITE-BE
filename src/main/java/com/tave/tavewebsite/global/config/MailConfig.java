a2578113bd88aac237ef8fdac684d84a508b07f0
koreaioi <koreaioi@gachon.ac.kr>
2024-11-04 19:00:03 +0900

    feat: 메일 전송을 위한 MailConfig 추가

---
+++ src/main/java/com/tave/tavewebsite/global/config/MailConfig.java
@@ -0,0 +1,49 @@
+package com.tave.tavewebsite.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.username}")
+    private String from;
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender mailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        // SMTP 서버명
+        javaMailSender.setHost("smtp.naver.com");
+        // 네이버 아이디
+        javaMailSender.setUsername(from);
+        // 네이버 비밀번호
+        javaMailSender.setPassword(password);
+        // SMTP 포트
+        javaMailSender.setPort(465);
+        // 메일 인증서버 가져오기
+        javaMailSender.setJavaMailProperties(getMailProperties());
+        javaMailSender.setDefaultEncoding("UTF-8");
+
+        return javaMailSender;
+    }
+
+    // 메일 인증서버 정보 가져오기
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
+        properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
+        properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
+        properties.setProperty("mail.debug", "true"); // 디버그 사용
+        properties.setProperty("mail.smtp.ssl.trust", "smtp.naver.com"); // ssl 인증 서버 (smtp 서버명)
+        properties.setProperty("mail.smtp.ssl.enable", "true"); // ssl 사용
+
+        return properties;
+    }
+}
