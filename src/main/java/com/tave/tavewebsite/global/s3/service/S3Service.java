e51f00ae0a3dd2bc7c6dceb2d52cf9f15f53609e
이준범 <jach1206@naver.com>
2024-11-05 16:31:03 +0900

    feat: S3 이미지 crud 기능 구현

--- src/main/java/com/tave/tavewebsite/global/s3/service/S3Service.java
+++ src/main/java/com/tave/tavewebsite/global/s3/service/S3Service.java
@@ -1,41 +1,64 @@
 package com.tave.tavewebsite.global.s3.service;
 
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import java.io.File;
+import com.tave.tavewebsite.global.s3.exception.S3ErrorMessage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Slf4j
 public class S3Service {
 
-    private static final String LOG_FORMAT = "Class : {}, Code : {}, Message : {}";
-
-    private final AmazonS3Client s3Client;
+    private final AmazonS3 s3Client;
     private final String bucketName;
 
-    public S3Service(AmazonS3Client s3Client, @Value("${BUCKET_NAME}") String bucketName) {
+    public S3Service(AmazonS3 s3Client, @Value("${bucket_name}") String bucketName) {
         this.s3Client = s3Client;
         this.bucketName = bucketName;
     }
 
-    public void uploadImages(File file) {
-        String key = file.getName();
+    public void uploadImages(MultipartFile file) {
+        String key = file.getOriginalFilename();
         if (s3Client.doesObjectExist(bucketName, key)) {
             s3Client.deleteObject(bucketName, key);
         }
-        s3Client.putObject(new PutObjectRequest(bucketName, key, file));
+        // MultipartFile에서 InputStream을 얻어 S3에 업로드합니다.
+        try (InputStream inputStream = file.getInputStream()) {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+
+            // PutObjectRequest 생성 시 InputStream과 ContentType을 설정합니다.
+            PutObjectRequest putRequest = new PutObjectRequest(bucketName, key, inputStream, metadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead);
+            s3Client.putObject(putRequest);
+        } catch (IOException e) {
+            throw new RuntimeException(S3ErrorMessage.UPLOAD_FAIL.getMessage());
+        }
     }
 
     public URL getImageUrl(String key) {
-        return s3Client.getUrl(bucketName, key);
+        try {
+            return s3Client.getUrl(bucketName, key);
+        } catch (Exception e) {
+            throw new RuntimeException(S3ErrorMessage.NOT_EXIST_NAME.getMessage());
+        }
     }
 
     public void deleteImage(String key) {
-        s3Client.deleteObject(bucketName, key);
+        try {
+            s3Client.deleteObject(bucketName, key);
+        } catch (Exception e) {
+            throw new RuntimeException(S3ErrorMessage.NOT_EXIST_NAME.getMessage());
+        }
     }
 
 }
