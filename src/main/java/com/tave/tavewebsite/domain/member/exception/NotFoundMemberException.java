4a37d4d22d7956f13191038b12d839d5ef3dc332
koreaioi <koreaioi@gachon.ac.kr>
2024-11-08 02:42:12 +0900

    fix: 예외명 User -> Member 변경

--- src/main/java/com/tave/tavewebsite/domain/member/exception/NotFoundMemberException.java
+++ src/main/java/com/tave/tavewebsite/domain/member/exception/NotFoundMemberException.java
@@ -4,8 +4,8 @@ import com.tave.tavewebsite.global.exception.BaseErrorException;
 
 import static com.tave.tavewebsite.domain.member.exception.ErrorMessage.NOT_FOUNT_USER;
 
-public class NotFoundUserException extends BaseErrorException {
-    public NotFoundUserException() {
+public class NotFoundMemberException extends BaseErrorException {
+    public NotFoundMemberException() {
         super(NOT_FOUNT_USER.getCode(), NOT_FOUNT_USER.getMessage());
     }
 }
