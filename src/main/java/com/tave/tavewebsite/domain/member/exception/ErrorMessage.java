10772a5c7f5be8c67e5ca76e133dddf06ce09e4b
koreaioi <koreaioi@gachon.ac.kr>
2024-11-08 04:15:36 +0900

    fix 닉네임 중복 에러 메세지 변경

--- src/main/java/com/tave/tavewebsite/domain/member/exception/ErrorMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/member/exception/ErrorMessage.java
@@ -9,7 +9,7 @@ public enum ErrorMessage {
 
     NOT_FOUNT_USER(400, "유저를 찾을 수 없습니다."),
     DUPLICATE_EMAIL(400, "이메일이 중복됩니다."),
-    DUPLICATE_NICKNAME(400, "ID가 중복됩니다.");
+    DUPLICATE_NICKNAME(400, "이미 사용중인 아이디입니다. 다른 아이디로 가입해주세요.");
 
     final int code;
     final String message;
