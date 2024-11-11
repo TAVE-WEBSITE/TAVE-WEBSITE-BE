2e1baaaefc6941ec9becccf3cd7704726e3de515
koreaioi <koreaioi@gachon.ac.kr>
2024-11-12 02:52:10 +0900

    fix: 에러 메세지 직관적으로 변경

--- src/main/java/com/tave/tavewebsite/domain/member/exception/ErrorMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/member/exception/ErrorMessage.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     NOT_FOUNT_USER(400, "유저를 찾을 수 없습니다."),
-    DUPLICATE_EMAIL(400, "이메일이 중복됩니다."),
+    DUPLICATE_EMAIL(400, "중복되는 이메일입니다."),
     DUPLICATE_NICKNAME(400, "이미 사용중인 아이디입니다. 다른 아이디로 가입해주세요.");
 
     final int code;
