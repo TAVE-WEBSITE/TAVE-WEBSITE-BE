ffed24c0f9863e9650d82b11a36ddee7ab777869
koreaioi <koreaioi@gachon.ac.kr>
2024-11-13 03:40:04 +0900

    feat: SessionRepository - 페이징 구현

--- src/main/java/com/tave/tavewebsite/domain/session/repository/SessionRepository.java
+++ src/main/java/com/tave/tavewebsite/domain/session/repository/SessionRepository.java
@@ -1,11 +1,14 @@
 package com.tave.tavewebsite.domain.session.repository;
 
 import com.tave.tavewebsite.domain.session.entity.Session;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
 
-    List<Session> findByGenerationAndIsPublic(String generation, boolean isPublic);
+    Page<Session> findByGenerationAndIsPublic(String generation, boolean isPublic, Pageable pageable);
+
 }
