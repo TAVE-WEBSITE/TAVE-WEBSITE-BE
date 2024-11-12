3260d35f1b0ab2aad7fca1f4a7cef4300af976a3
wjd4204 <brian876373@gmail.com>
2024-11-12 15:43:39 +0900

    feat: Study 엔티티 속성 수정

--- src/main/java/com/tave/tavewebsite/domain/study/entity/Study.java
+++ src/main/java/com/tave/tavewebsite/domain/study/entity/Study.java
@@ -1,6 +1,7 @@
 package com.tave.tavewebsite.domain.study.entity;
 
 
+import com.tave.tavewebsite.domain.study.dto.StudyReq;
 import com.tave.tavewebsite.global.common.BaseEntity;
 import com.tave.tavewebsite.global.common.FieldType;
 import jakarta.persistence.*;
@@ -10,8 +11,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.URL;
 
+import java.net.URL;
 import java.time.LocalDateTime;
 
 
@@ -54,22 +55,32 @@ public class Study extends BaseEntity {
     private LocalDateTime endDate;
 
     @NotNull
-    @URL
     @Column(length = 2083, nullable = false) // DDL varchar(2083)
     private String blogUrl;
 
     @NotNull
-    @URL
     @Column(length = 2083, nullable = false) // DDL varchar(2083)
     private String imgUrl;
 
     @Builder
-    public Study(String teamName, String topic, String generation, FieldType field, String blogUrl, String imgUrl) {
-        this.topic = topic;
-        this.teamName = teamName;
-        this.generation = generation;
-        this.field = field;
-        this.blogUrl = blogUrl;
-        this.imgUrl = imgUrl;
+    public Study(StudyReq req, URL imageUrl) {
+        this.topic = req.topic();
+        this.teamName = req.teamName();
+        this.generation = req.generation();
+        this.field = FieldType.valueOf(req.field());
+        this.blogUrl = req.blogUrl();
+        this.imgUrl = imageUrl.toString();
     }
+
+    public Study updateStudy(StudyReq req, URL imageUrl) {
+        this.topic = req.topic();
+        this.teamName = req.teamName();
+        this.generation = req.generation();
+        this.field = FieldType.valueOf(req.field());
+        this.blogUrl = req.blogUrl();
+        this.imgUrl = imageUrl.toString();
+        return this;
+    }
+
+
 }
