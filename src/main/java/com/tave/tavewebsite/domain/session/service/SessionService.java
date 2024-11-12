38fe235487e6be27b806821860d497080083b000
koreaioi <koreaioi@gachon.ac.kr>
2024-11-13 03:39:29 +0900

    feat: 서비스 - 기수별 세션 조회

--- src/main/java/com/tave/tavewebsite/domain/session/service/SessionService.java
+++ src/main/java/com/tave/tavewebsite/domain/session/service/SessionService.java
@@ -3,14 +3,18 @@ package com.tave.tavewebsite.domain.session.service;
 import com.tave.tavewebsite.domain.session.entity.Session;
 import com.tave.tavewebsite.domain.session.dto.request.SessionRequestDto;
 import com.tave.tavewebsite.domain.session.dto.response.SessionResponseDto;
+import com.tave.tavewebsite.domain.session.exception.SessionNotFoundException;
 import com.tave.tavewebsite.domain.session.repository.SessionRepository;
 import com.tave.tavewebsite.global.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URL;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,14 +25,18 @@ public class SessionService {
     private final S3Service s3Service;
 
     public SessionResponseDto saveSession(SessionRequestDto sessionRequestDto, MultipartFile file) {
-
-        // 이미지 업로드 후 저장
         URL savedImageUrl = s3Service.uploadImages(file);
-        log.info(savedImageUrl.toString());
-
-        // RequestDto -> Entity -> save() -> ResponseDto
         Session savedSession = sessionRepository.save(Session.of(sessionRequestDto, savedImageUrl));
         return SessionResponseDto.from(savedSession);
     }
+
+    public List<SessionResponseDto> findSessionByGeneration(String generation, int page, int size) {
+        return sessionRepository.findByGenerationAndIsPublic(generation, true, PageRequest.of(page- 1, size))
+                .getContent()
+                .stream()
+                .map(SessionResponseDto::from)
+                .toList();
+    }
+
 
 }
