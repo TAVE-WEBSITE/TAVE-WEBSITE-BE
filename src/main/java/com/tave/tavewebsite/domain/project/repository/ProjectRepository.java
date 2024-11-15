ddf4ece97c88b33dda057a8f091662cf4163ef0e
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-11-15 14:53:10 +0900

    feat : repository 파일 생성

---
+++ src/main/java/com/tave/tavewebsite/domain/project/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.tave.tavewebsite.domain.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository {
+
+}
