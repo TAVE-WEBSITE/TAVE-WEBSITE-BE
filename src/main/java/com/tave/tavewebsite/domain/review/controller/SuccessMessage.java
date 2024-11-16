3e3f02cc7e575098ab8202449f3dc0a20e3e409c
koreaioi <koreaioi@gachon.ac.kr>
2024-11-16 23:31:02 +0900

    feat: 리뷰 SuccessMessage 수정 추가

--- src/main/java/com/tave/tavewebsite/domain/review/controller/SuccessMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/review/controller/SuccessMessage.java
@@ -7,11 +7,16 @@ import lombok.Getter;
 public enum SuccessMessage {
 
     REVIEW_CREATE("후기를 생성합니다."),
-    REVIEW_GET("후기를 반환합니다.");
+    REVIEW_GET("후기를 반환합니다."),
+    REVIEW_UPDATE("후기를 수정했습니다.");
 
     private final String message;
 
     public String getMessage(String generation) {
         return generation +" " + message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
