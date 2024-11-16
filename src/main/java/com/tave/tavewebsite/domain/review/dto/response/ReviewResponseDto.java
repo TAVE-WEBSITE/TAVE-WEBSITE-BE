a851f344aee6ec265397f108bf7e7d81512acf25
koreaioi <koreaioi@gachon.ac.kr>
2024-11-16 19:25:31 +0900

    feat: 요청 응답 Dto 구현

---
+++ src/main/java/com/tave/tavewebsite/domain/review/dto/response/ReviewResponseDto.java
@@ -0,0 +1,14 @@
+package com.tave.tavewebsite.domain.review.dto.response;
+
+
+import com.tave.tavewebsite.global.common.FieldType;
+import lombok.Builder;
+
+@Builder
+public record ReviewResponseDto(
+        String nickname,
+        String generation,
+        FieldType field,
+        String content
+) {
+}
