b853c50cce6b72666e758573ce5e63cddf57c871
koreaioi <koreaioi@gachon.ac.kr>
2024-11-16 19:25:54 +0900

    feat: Dto <-> Entity 사이 Mapper 구현

---
+++ src/main/java/com/tave/tavewebsite/domain/review/mapper/ReviewMapper.java
@@ -0,0 +1,32 @@
+package com.tave.tavewebsite.domain.review.mapper;
+
+import com.tave.tavewebsite.domain.review.dto.request.ReviewRequestDto;
+import com.tave.tavewebsite.domain.review.dto.response.ReviewResponseDto;
+import com.tave.tavewebsite.domain.review.entity.Review;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReviewMapper {
+
+    public Review toReview(ReviewRequestDto reviewRequestDto) {
+        return Review.builder()
+                .nickname(reviewRequestDto.nickname())
+                .generation(reviewRequestDto.generation())
+                .field(reviewRequestDto.field())
+                .content(reviewRequestDto.content())
+                .isPublic(reviewRequestDto.isPublic())
+                .build();
+    }
+
+    public ReviewResponseDto toReviewResponseDto(Review review) {
+        return ReviewResponseDto.builder()
+                .nickname(review.getNickname())
+                .generation(review.getGeneration())
+                .field(review.getField())
+                .content(review.getContent())
+                .build();
+    }
+
+
+
+}
