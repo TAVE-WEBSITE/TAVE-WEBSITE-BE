ac320321659734ffd6adc4c7027b3053b5d9868d
koreaioi <koreaioi@gachon.ac.kr>
2024-11-16 20:22:48 +0900

    feat: ReviewRepository - 기수별 조회

--- src/main/java/com/tave/tavewebsite/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/tave/tavewebsite/domain/review/repository/ReviewRepository.java
@@ -3,5 +3,9 @@ package com.tave.tavewebsite.domain.review.repository;
 import com.tave.tavewebsite.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByGenerationAndIsPublic(String generation, boolean isPublic);
 }
