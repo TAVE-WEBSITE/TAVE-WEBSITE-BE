ba3e9a0229aaca95bae4fa512a34d17da392b0fe
koreaioi <koreaioi@gachon.ac.kr>
2024-11-16 20:24:49 +0900

    feat: SuccessMessage - 응답 성공 시, 기수별 메세지 반환

--- src/main/java/com/tave/tavewebsite/domain/review/controller/SuccessMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/review/controller/SuccessMessage.java
@@ -3,11 +3,15 @@ package com.tave.tavewebsite.domain.review.controller;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
 public enum SuccessMessage {
 
-    REVIEW_CREATE("후기를 생성했습니다.");
+    REVIEW_CREATE("후기를 생성합니다."),
+    REVIEW_GET("후기를 반환합니다.");
 
     private final String message;
+
+    public String getMessage(String generation) {
+        return generation +" " + message;
+    }
 }
