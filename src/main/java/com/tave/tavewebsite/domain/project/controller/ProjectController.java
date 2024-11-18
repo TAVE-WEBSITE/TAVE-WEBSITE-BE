8c2b9c61b4910d42b15d95186a977f9000bf3032
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-11-19 02:37:00 +0900

    feat : project 생성 코드 작성

--- src/main/java/com/tave/tavewebsite/domain/project/controller/ProjectController.java
+++ src/main/java/com/tave/tavewebsite/domain/project/controller/ProjectController.java
@@ -1,7 +1,13 @@
 package com.tave.tavewebsite.domain.project.controller;
 
+import com.tave.tavewebsite.domain.project.dto.request.ProjectRequestDto;
+import com.tave.tavewebsite.domain.project.dto.response.ProjectResponseDto;
 import com.tave.tavewebsite.domain.project.service.ProjectService;
+import com.tave.tavewebsite.global.success.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/project")
 @RequiredArgsConstructor
 public class ProjectController {
+
     private final ProjectService projectService;
 
+    @PostMapping
+    public SuccessResponse<ProjectResponseDto> createProject(@RequestBody @Valid ProjectRequestDto requestDto) {
+        ProjectResponseDto response = projectService.createProject(requestDto);
+        return new SuccessResponse<>(response);
+    }
 }
--- src/main/java/com/tave/tavewebsite/domain/project/service/ProjectService.java
+++ src/main/java/com/tave/tavewebsite/domain/project/service/ProjectService.java
@@ -1,5 +1,8 @@
 package com.tave.tavewebsite.domain.project.service;
 
+import com.tave.tavewebsite.domain.project.dto.request.ProjectRequestDto;
+import com.tave.tavewebsite.domain.project.dto.response.ProjectResponseDto;
+import com.tave.tavewebsite.domain.project.entity.Project;
 import com.tave.tavewebsite.domain.project.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
+
+    public ProjectResponseDto createProject(ProjectRequestDto requestDto) {
+        Project project = Project.builder()
+                .title(requestDto.getTitle())
+                .description(requestDto.getDescription())
+                .generation(requestDto.getGeneration())
+                .teamName(requestDto.getTeamName())
+                .field(requestDto.getField())
+                .blogUrl(requestDto.getBlogUrl())
+                .imgUrl(requestDto.getImgUrl())
+                .build();
+        projectRepository.save(project);
+        return new ProjectResponseDto(project);
+    }
 
 }
