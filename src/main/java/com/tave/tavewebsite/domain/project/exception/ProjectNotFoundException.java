b38f361ffa30ec611b64e5e8a62dddb904974fe7
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-11-19 20:02:46 +0900

    feat : project errorcode 추가

---
+++ src/main/java/com/tave/tavewebsite/domain/project/exception/ProjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.tave.tavewebsite.domain.project.exception;
+
+import com.tave.tavewebsite.global.exception.BaseErrorException;
+
+import static com.tave.tavewebsite.domain.project.exception.ErrorMessage.PROJECT_NOT_FOUND;
+
+public class ProjectNotFoundException extends BaseErrorException {
+    public ProjectNotFoundException(String message) {
+        super(PROJECT_NOT_FOUND.getCode(), PROJECT_NOT_FOUND.getMessage());
+    }
+}
