eec947cb99f71386087e659b290cb6625b80a27d
이준범 <jach1206@naver.com>
2024-11-21 23:35:07 +0900

    feat: 로그인과 관련된 코드 작성
    - CustomUserDetailsService: 데이터베이스에서 유저가 존재하는지 파악하여 유저의 인증 정보를 반환하는 클래스이다.
    
    - Member: 비밀번호를 인코딩하여 저장하는 로직을 추가했다.
    
    - MemberService: 로그인 진행 메서드를 추가했다.
    
    - UsernamePwdAuthenticationProvider: UserDetails를 호출하기 위한 Provider를 정의했다.

--- src/main/java/com/tave/tavewebsite/domain/member/service/MemberService.java
+++ src/main/java/com/tave/tavewebsite/domain/member/service/MemberService.java
@@ -1,21 +1,26 @@
 package com.tave.tavewebsite.domain.member.service;
 
+import static com.tave.tavewebsite.domain.member.entity.RoleType.UNAUTHORIZED_MANAGER;
+
 import com.tave.tavewebsite.domain.member.dto.request.RegisterManagerRequestDto;
+import com.tave.tavewebsite.domain.member.dto.request.SignUpRequestDto;
 import com.tave.tavewebsite.domain.member.dto.response.UnauthorizedManagerResponseDto;
 import com.tave.tavewebsite.domain.member.entity.Member;
-import com.tave.tavewebsite.domain.member.entity.RoleType;
 import com.tave.tavewebsite.domain.member.exception.DuplicateEmailException;
 import com.tave.tavewebsite.domain.member.exception.DuplicateNicknameException;
 import com.tave.tavewebsite.domain.member.memberRepository.MemberRepository;
-import com.tave.tavewebsite.global.mail.service.MailService;
 import com.tave.tavewebsite.global.mail.dto.MailResponseDto;
+import com.tave.tavewebsite.global.mail.service.MailService;
+import com.tave.tavewebsite.global.security.entity.JwtToken;
+import com.tave.tavewebsite.global.security.utils.JwtTokenProvider;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static com.tave.tavewebsite.domain.member.entity.RoleType.UNAUTHORIZED_MANAGER;
 
 @Service
 @Transactional
@@ -24,35 +29,50 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final MailService mailService;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
 
-    public MailResponseDto saveMember(RegisterManagerRequestDto requestDto){
+    public MailResponseDto saveMember(RegisterManagerRequestDto requestDto) {
 
         validateNickname(requestDto.nickname());
         validateEmail(requestDto.email());
 
-        // PasswordEncoder 추가되면
-        // toMember에서 encode(requestDto.getPassword()) 추가하기
-        // 추가 후 위 주석은 삭제
-        Member saveMember = memberRepository.save(Member.toMember(requestDto));
+        Member saveMember = memberRepository.save(Member.toMember(requestDto, passwordEncoder));
         return mailService.sendManagerRegisterMessage(saveMember.getEmail());
     }
 
     @Transactional(readOnly = true)
-    public List<UnauthorizedManagerResponseDto>  getUnauthorizedManager(){
+    public List<UnauthorizedManagerResponseDto> getUnauthorizedManager() {
         return memberRepository.findByRole(UNAUTHORIZED_MANAGER).stream()
                 .map(UnauthorizedManagerResponseDto::fromEntity)
                 .toList();
     }
 
-    private void validateEmail(String email){
+    public JwtToken signIn(SignUpRequestDto requestDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                requestDto.email(),
+                requestDto.password());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        return jwtTokenProvider.generateToken(authentication);
+    }
+
+    private void validateEmail(String email) {
         memberRepository.findByEmail(email).ifPresent(
-                member -> {throw new DuplicateEmailException();}
+                member -> {
+                    throw new DuplicateEmailException();
+                }
         );
     }
 
-    public void validateNickname(String nickname){
+    public void validateNickname(String nickname) {
         memberRepository.findByNickname(nickname).ifPresent(
-                member -> {throw new DuplicateNicknameException();}
+                member -> {
+                    throw new DuplicateNicknameException();
+                }
         );
     }
 
---
+++ src/main/java/com/tave/tavewebsite/global/security/utils/UsernamePwdAuthenticationProvider.java
@@ -0,0 +1,53 @@
+package com.tave.tavewebsite.global.security.utils;
+
+import com.tave.tavewebsite.domain.member.entity.Member;
+import com.tave.tavewebsite.domain.member.memberRepository.MemberRepository;
+import com.tave.tavewebsite.global.security.exception.LoginFailException.AuthenticationNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private MemberRepository customerRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        Optional<Member> customer = customerRepository.findByEmail(username);
+
+        if (customer.isPresent()) {
+            if (passwordEncoder.matches(pwd, customer.get().getPassword())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.get().getRole().name()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+
+            } else {
+                throw new AuthenticationNotFoundException();
+            }
+        } else {
+            throw new AuthenticationNotFoundException();
+        }
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
