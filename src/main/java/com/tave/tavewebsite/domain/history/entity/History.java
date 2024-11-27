e9f59518b530fad840a375681c6817f9b4b9fd03
이준범 <jach1206@naver.com>
2024-11-27 20:40:05 +0900

    feat: 동아리 이력 CRUD 개발
    - 일반 회원의 조회 api는 NormalHistoryController에 구현했다.
    
    - 관리자의 조회 api는 ManagerHistoryController에 구현했다.

--- src/main/java/com/tave/tavewebsite/domain/history/entity/History.java
+++ src/main/java/com/tave/tavewebsite/domain/history/entity/History.java
@@ -1,7 +1,13 @@
 package com.tave.tavewebsite.domain.history.entity;
 
+import com.tave.tavewebsite.domain.history.dto.request.HistoryRequestDto;
 import com.tave.tavewebsite.global.common.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -40,5 +46,11 @@ public class History extends BaseEntity {
         this.generation = generation;
         this.description = description;
         this.isPublic = isPublic;
+    }
+
+    public void putHistory(HistoryRequestDto historyResponseDto) {
+        generation = historyResponseDto.generation();
+        description = historyResponseDto.description();
+        isPublic = historyResponseDto.isPublic();
     }
 }
---
+++ src/main/java/com/tave/tavewebsite/domain/history/service/HistoryService.java
@@ -0,0 +1,54 @@
+package com.tave.tavewebsite.domain.history.service;
+
+import com.tave.tavewebsite.domain.history.dto.request.HistoryRequestDto;
+import com.tave.tavewebsite.domain.history.dto.response.HistoryResponseDto;
+import com.tave.tavewebsite.domain.history.entity.History;
+import com.tave.tavewebsite.domain.history.exception.HistoryErrorException.HistoryNotFoundException;
+import com.tave.tavewebsite.domain.history.repository.HistoryRepository;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class HistoryService {
+
+    private final HistoryRepository historyRepository;
+
+    @Transactional(readOnly = true)
+    public List<HistoryResponseDto> findPublicOrderByGenerationDesc() {
+        List<History> isPublicOrderByGenerationDesc = historyRepository.findByIsPublicOrderByGenerationDesc(true);
+        List<HistoryResponseDto> historyResponseDtos = new ArrayList<>();
+        for (History history : isPublicOrderByGenerationDesc) {
+            historyResponseDtos.add(HistoryResponseDto.of(history));
+        }
+        return historyResponseDtos;
+    }
+
+    @Transactional(readOnly = true)
+    public List<HistoryResponseDto> findAllOrderByGenerationDesc() {
+        List<History> orderByGenerationDesc = historyRepository.findAllByOrderByGenerationDesc();
+        List<HistoryResponseDto> historyResponseDtos = new ArrayList<>();
+        for (History history : orderByGenerationDesc) {
+            historyResponseDtos.add(HistoryResponseDto.of(history));
+        }
+        return historyResponseDtos;
+    }
+
+    public void save(HistoryRequestDto dto) {
+        historyRepository.save(dto.toHistory());
+    }
+
+    @Transactional
+    public void put(Long id, HistoryRequestDto dto) {
+        History history = historyRepository.findById(id).orElseThrow(HistoryNotFoundException::new);
+        history.putHistory(dto);
+    }
+
+    public void delete(Long id) {
+        historyRepository.findById(id).orElseThrow(HistoryNotFoundException::new);
+        historyRepository.deleteById(id);
+    }
+}
