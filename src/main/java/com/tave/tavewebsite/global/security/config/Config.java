baca379f6ab93b93d00f52aee4b72d69ebd98069
이준범 <jach1206@naver.com>
2024-11-27 20:48:10 +0900

    fix: csrf 토큰 비활성화
    - jwt 토큰은 쿠키에 인증정보를 저장하지 않기 때문에, csrf 토큰을 활성화 할 필요가 없어 비활성화 처리했다.

--- src/main/java/com/tave/tavewebsite/global/security/config/Config.java
+++ src/main/java/com/tave/tavewebsite/global/security/config/Config.java
@@ -75,7 +75,8 @@ public class Config {
 
                         // 운영진 전용 api
                         .requestMatchers("/v1/manager/**")
-                        .hasAnyRole(RoleType.MANAGER.name(), RoleType.ADMIN.name())
+                        .hasAnyRole(RoleType.MANAGER.name(), RoleType.ADMIN.name(),
+                                RoleType.UNAUTHORIZED_MANAGER.name())
 
                         // 회장 전용 api
                         .requestMatchers("/v1/admin/**").hasRole(RoleType.ADMIN.name())
