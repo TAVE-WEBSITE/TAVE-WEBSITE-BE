a1a07e57ddfb33c0a2a542049193b8e2bad4fe8b
이준범 <jach1206@naver.com>
2024-11-29 16:21:05 +0900

    hotfix: resolve conflict
    - 브랜치 머지 중 충돌이 발생에 의해 엔드포인트가 겹치는 api가 생겼다.
    이를 없애주었다.

--- src/main/java/com/tave/tavewebsite/domain/member/controller/ManagerController.java
+++ src/main/java/com/tave/tavewebsite/domain/member/controller/ManagerController.java
@@ -1,17 +1,21 @@
 package com.tave.tavewebsite.domain.member.controller;
 
-import com.tave.tavewebsite.domain.member.dto.request.*;
+import com.tave.tavewebsite.domain.member.dto.request.ResetPasswordReq;
+import com.tave.tavewebsite.domain.member.dto.request.ValidateEmailReq;
 import com.tave.tavewebsite.domain.member.dto.response.CheckNickNameResponseDto;
-import com.tave.tavewebsite.domain.member.dto.response.SignInResponseDto;
 import com.tave.tavewebsite.domain.member.dto.response.UnauthorizedManagerResponseDto;
 import com.tave.tavewebsite.domain.member.service.MemberService;
-import com.tave.tavewebsite.global.mail.dto.MailResponseDto;
-import com.tave.tavewebsite.global.security.entity.JwtToken;
 import com.tave.tavewebsite.global.success.SuccessResponse;
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1")
@@ -19,12 +23,6 @@ import org.springframework.web.bind.annotation.*;
 public class ManagerController {
 
     private final MemberService memberService;
-
-    @PostMapping("/auth/signup")
-    public SuccessResponse<MailResponseDto> registerManager(@RequestBody @Valid RegisterManagerRequestDto requestDto) {
-        MailResponseDto response = memberService.saveMember(requestDto);
-        return new SuccessResponse<>(response);
-    }
 
     @PostMapping("/normal/authenticate/email")
     public SuccessResponse sendEmail(@RequestBody ValidateEmailReq requestDto) {
@@ -46,24 +44,6 @@ public class ManagerController {
         memberService.updateAuthentication(memberId);
 
         return new SuccessResponse("update Success.");
-    }
-
-    @PostMapping("/auth/signin")
-    public SuccessResponse<SignInResponseDto> signIn(@RequestBody SignUpRequestDto requestDto) {
-        SignInResponseDto signInResponseDto = memberService.signIn(requestDto);
-        return new SuccessResponse<>(signInResponseDto);
-    }
-
-    @PostMapping("/refresh")
-    public SuccessResponse<JwtToken> refreshToken(@RequestBody RefreshTokenRequestDto requestDto) {
-        JwtToken jwtToken = memberService.refreshToken(requestDto);
-        return new SuccessResponse<>(jwtToken);
-    }
-
-    @GetMapping("/signOut")
-    public SuccessResponse signOut(@RequestHeader("Authorization") String token) {
-        memberService.singOut(token);
-        return SuccessResponse.ok();
     }
 
     @GetMapping("/admin/unauthorized")
