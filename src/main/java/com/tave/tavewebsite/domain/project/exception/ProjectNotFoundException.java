cccb229a911557916faa0fe1636ecec570735dbf
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-12-02 00:56:46 +0900

    fix: 매개변수 삭제

--- src/main/java/com/tave/tavewebsite/domain/project/exception/ProjectNotFoundException.java
+++ src/main/java/com/tave/tavewebsite/domain/project/exception/ProjectNotFoundException.java
@@ -5,7 +5,7 @@ import com.tave.tavewebsite.global.exception.BaseErrorException;
 import static com.tave.tavewebsite.domain.project.exception.ErrorMessage.PROJECT_NOT_FOUND;
 
 public class ProjectNotFoundException extends BaseErrorException {
-    public ProjectNotFoundException(String message) {
+    public ProjectNotFoundException() {
         super(PROJECT_NOT_FOUND.getCode(), PROJECT_NOT_FOUND.getMessage());
     }
 }
