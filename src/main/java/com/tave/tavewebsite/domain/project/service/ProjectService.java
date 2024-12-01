e61850e181940c19e4aa4b96e8e7e3a85d740c12
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-12-01 21:27:01 +0900

    fix: S3 이미지 업로드 기능

--- src/main/java/com/tave/tavewebsite/domain/project/service/ProjectService.java
+++ src/main/java/com/tave/tavewebsite/domain/project/service/ProjectService.java
@@ -5,10 +5,13 @@ import com.tave.tavewebsite.domain.project.dto.response.ProjectResponseDto;
 import com.tave.tavewebsite.domain.project.entity.Project;
 import com.tave.tavewebsite.domain.project.exception.ProjectNotFoundException;
 import com.tave.tavewebsite.domain.project.repository.ProjectRepository;
+import com.tave.tavewebsite.global.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,8 +20,10 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final S3Service s3Service;
 
-    public ProjectResponseDto createProject(ProjectRequestDto requestDto) {
+    public ProjectResponseDto createProject(ProjectRequestDto requestDto, MultipartFile imageFile) {
+        URL imgUrl = s3Service.uploadImages(imageFile);
         Project project = Project.builder()
                 .title(requestDto.getTitle())
                 .description(requestDto.getDescription())
@@ -26,7 +31,7 @@ public class ProjectService {
                 .teamName(requestDto.getTeamName())
                 .field(requestDto.getField())
                 .blogUrl(requestDto.getBlogUrl())
-                .imgUrl(requestDto.getImgUrl())
+                .imgUrl(imgUrl.toString())
                 .build();
         projectRepository.save(project);
         return new ProjectResponseDto(project);
@@ -43,16 +48,17 @@ public class ProjectService {
     @Transactional(readOnly = true)
     public ProjectResponseDto getProjectById(Long projectId) {
         Project project = projectRepository.findById(projectId)
-                .orElseThrow(() -> new ProjectNotFoundException("Project ID: " + projectId + projectId));
+                .orElseThrow(() -> new ProjectNotFoundException("Project ID: " + projectId));
         return new ProjectResponseDto(project);
     }
 
-    public ProjectResponseDto updateProject(Long projectId, ProjectRequestDto requestDto) {
+    public ProjectResponseDto updateProject(Long projectId, ProjectRequestDto requestDto, MultipartFile imageFile) {
         Project project = projectRepository.findById(projectId)
                 .orElseThrow(() -> new ProjectNotFoundException("Project ID: " + projectId));
 
+        URL imgUrl = s3Service.uploadImages(imageFile);
         project.update(requestDto.getTitle(), requestDto.getDescription(), requestDto.getGeneration(),
-                requestDto.getTeamName(), requestDto.getField(), requestDto.getBlogUrl(), requestDto.getImgUrl());
+                requestDto.getTeamName(), requestDto.getField(), requestDto.getBlogUrl(), imgUrl.toString());
 
         return new ProjectResponseDto(project);
     }
@@ -60,6 +66,7 @@ public class ProjectService {
     public void deleteProject(Long projectId) {
         Project project = projectRepository.findById(projectId)
                 .orElseThrow(() -> new ProjectNotFoundException("Project ID: " + projectId));
+        s3Service.deleteImage(project.getImgUrl());
         projectRepository.delete(project);
     }
 }
