bbc354de74c59259093b355d0b92d90ebfd0630e
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-12-03 16:19:56 +0900

    feat: MANAGER 권한 아닌 경우 예외

--- src/main/java/com/tave/tavewebsite/domain/member/exception/ErrorMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/member/exception/ErrorMessage.java
@@ -12,7 +12,8 @@ public enum ErrorMessage {
     DUPLICATE_NICKNAME(400, "이미 사용중인 아이디입니다. 다른 아이디로 가입해주세요."),
     _NOT_MATCHED_VERIFIED_NUMBER(400, "인증 번호가 일치하지 않습니다."),
     _EXPIRED_NUMBER(401, "인증번호의 만료시간이 지났습니다."),
-    _NOT_MATCHED_PASSWORD(400, "비밀번호가 일치하지 않습니다.");
+    _NOT_MATCHED_PASSWORD(400, "비밀번호가 일치하지 않습니다."),
+    NOT_MANAGER(400, "MANAGER이 아닙니다.");
 
     final int code;
     final String message;
