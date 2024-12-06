1edf215f713b53393c726dab2e5fadfbde2520d7
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-12-07 00:35:10 +0900

    feat: 응답메시지 추가

---
+++ src/main/java/com/tave/tavewebsite/domain/member/controller/SuccessMessage.java
@@ -0,0 +1,21 @@
+package com.tave.tavewebsite.domain.member.controller;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum SuccessMessage {
+    UNAUTHORIZED_MEMBER_READ("승인 대기 중인 운영진을 조회했습니다."),
+    AUTHORIZED_MEMBER_READ("승인된 운영진을 조회했습니다."),
+    MANAGER_DELETE("해당 운영진을 삭제했습니다."),
+    ALL_MANAGER_READ("모든 운영진을 조회했습니다.");
+
+    private final String message;
+
+    public String getMessage(String generation) {
+        return generation +" " + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
