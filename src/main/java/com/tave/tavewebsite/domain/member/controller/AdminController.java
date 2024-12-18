670b6e34a8601997276460b3005cf74ffe8e8210
이준범 <jach1206@naver.com>
2024-12-18 17:25:18 +0900

    style: 성공 Response 메시지 수정

--- src/main/java/com/tave/tavewebsite/domain/member/controller/AdminController.java
+++ src/main/java/com/tave/tavewebsite/domain/member/controller/AdminController.java
@@ -6,7 +6,11 @@ import com.tave.tavewebsite.domain.member.service.AdminService;
 import com.tave.tavewebsite.global.success.SuccessResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/admin")
@@ -18,19 +22,19 @@ public class AdminController {
     @GetMapping("/unauthorized")
     public SuccessResponse<List<UnauthorizedManagerResponseDto>> getUnauthorizedManager() {
         List<UnauthorizedManagerResponseDto> response = adminService.getUnauthorizedManager();
-        return new SuccessResponse<>(response, SuccessMessage.UNAUTHORIZED_MEMBER_READ.getMessage());
+        return new SuccessResponse<>(response, MemberSuccessMessage.UNAUTHORIZED_MEMBER_READ.getMessage());
     }
 
     @GetMapping("/authorized")
     public SuccessResponse<List<AuthorizedManagerResponseDto>> getAuthorizedAdmins() {
         List<AuthorizedManagerResponseDto> response = adminService.getAuthorizedAdmins();
-        return new SuccessResponse<>(response, SuccessMessage.AUTHORIZED_MEMBER_READ.getMessage());
+        return new SuccessResponse<>(response, MemberSuccessMessage.AUTHORIZED_MEMBER_READ.getMessage());
     }
 
     @DeleteMapping("/{memberId}")
     public SuccessResponse deleteManager(@PathVariable Long memberId) {
         adminService.deleteManager(memberId);
-        return SuccessResponse.ok(SuccessMessage.MANAGER_DELETE.getMessage());
+        return SuccessResponse.ok(MemberSuccessMessage.MANAGER_DELETE.getMessage());
     }
 
 }
--- src/main/java/com/tave/tavewebsite/domain/member/controller/MemberSuccessMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/member/controller/MemberSuccessMessage.java
@@ -3,15 +3,17 @@ package com.tave.tavewebsite.domain.member.controller;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public enum SuccessMessage {
+public enum MemberSuccessMessage {
     UNAUTHORIZED_MEMBER_READ("승인 대기 중인 운영진을 조회했습니다."),
     AUTHORIZED_MEMBER_READ("승인된 운영진을 조회했습니다."),
-    MANAGER_DELETE("해당 운영진을 삭제했습니다.");
+    MANAGER_DELETE("해당 운영진을 삭제했습니다."),
+    SIGN_OUT_SUCCESS("로그아웃에 성공했습니다."),
+    DELETE_MEMBER_SUCCESS("회원탈퇴에 성공했습니다.");
 
     private final String message;
 
     public String getMessage(String generation) {
-        return generation +" " + message;
+        return generation + " " + message;
     }
 
     public String getMessage() {
