1e4fa7a7ec00454397b903a2a5253a7c92f2015e
이준범 <jach1206@naver.com>
2024-12-18 17:25:18 +0900

    style: 성공 Response 메시지 수정

--- src/main/java/com/tave/tavewebsite/domain/member/controller/AdminController.java
+++ src/main/java/com/tave/tavewebsite/domain/member/controller/AdminController.java
@@ -1,13 +1,16 @@
 package com.tave.tavewebsite.domain.member.controller;
 
-import com.tave.tavewebsite.domain.member.dto.response.ManagerResponseDto;
+import com.tave.tavewebsite.domain.member.dto.response.AuthorizedManagerResponseDto;
+import com.tave.tavewebsite.domain.member.dto.response.UnauthorizedManagerResponseDto;
 import com.tave.tavewebsite.domain.member.service.AdminService;
 import com.tave.tavewebsite.global.success.SuccessResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/admin")
@@ -16,17 +19,22 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    @GetMapping("/manager")
-    public SuccessResponse<Page<ManagerResponseDto>> getManagers(
-            @RequestParam(defaultValue = "ALL", name = "status") String status,
-            @PageableDefault(size = 8) Pageable pageable) {
-        return adminService.getManagersByStatus(status, pageable);
+    @GetMapping("/unauthorized")
+    public SuccessResponse<List<UnauthorizedManagerResponseDto>> getUnauthorizedManager() {
+        List<UnauthorizedManagerResponseDto> response = adminService.getUnauthorizedManager();
+        return new SuccessResponse<>(response, MemberSuccessMessage.UNAUTHORIZED_MEMBER_READ.getMessage());
+    }
+
+    @GetMapping("/authorized")
+    public SuccessResponse<List<AuthorizedManagerResponseDto>> getAuthorizedAdmins() {
+        List<AuthorizedManagerResponseDto> response = adminService.getAuthorizedAdmins();
+        return new SuccessResponse<>(response, MemberSuccessMessage.AUTHORIZED_MEMBER_READ.getMessage());
     }
 
     @DeleteMapping("/{memberId}")
     public SuccessResponse deleteManager(@PathVariable Long memberId) {
         adminService.deleteManager(memberId);
-        return SuccessResponse.ok(SuccessMessage.MANAGER_DELETE.getMessage());
+        return SuccessResponse.ok(MemberSuccessMessage.MANAGER_DELETE.getMessage());
     }
 
 }
