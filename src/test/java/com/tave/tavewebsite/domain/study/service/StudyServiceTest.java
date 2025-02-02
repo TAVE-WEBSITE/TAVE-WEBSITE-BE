3feafda6e9307dfa65d1f1ec3154674a9a51577f
wjd4204 <brian876373@gmail.com>
2025-02-02 17:45:58 +0900

    feat: 스터디 조회 테스트 코드 작성

--- src/test/java/com/tave/tavewebsite/domain/study/service/StudyServiceTest.java
+++ src/test/java/com/tave/tavewebsite/domain/study/service/StudyServiceTest.java
@@ -1,26 +1,36 @@
 package com.tave.tavewebsite.domain.study.service;
 
 import com.tave.tavewebsite.domain.study.dto.StudyRequestDto;
+import com.tave.tavewebsite.domain.study.dto.StudyResponseDto;
 import com.tave.tavewebsite.domain.study.entity.Study;
 import com.tave.tavewebsite.domain.study.repository.StudyRepository;
 import com.tave.tavewebsite.global.s3.service.S3Service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
     @InjectMocks
@@ -35,9 +45,16 @@ class StudyServiceTest {
     @Captor
     private ArgumentCaptor<Study> studyCaptor;
 
+    private List<StudyResponseDto> studyList;
+
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        studyList = new ArrayList<>();
+
+        for(int i=1;i<=100;i++){
+            studyList.add(new StudyResponseDto((long) i, "teamName" + i, "14",
+                    "BACKEND", "topic" + i, "image", "blog"));
+        }
     }
 
     @DisplayName("스터디가 성공적으로 생성된다.")
@@ -68,4 +85,24 @@ class StudyServiceTest {
 
         assertThat(savedStudy.getTeamName()).isEqualTo(req.teamName());
     }
+
+    @DisplayName("스터디를 파라미터에 따라 조회합니다.")
+    @Test
+    void getStudy() {
+     //given
+        Pageable pageable = PageRequest.of(0, 20);
+        Page<StudyResponseDto> responseDtos = new PageImpl<>(studyList);
+        String generation = "ALL";
+        String field = "ALL";
+
+     //when
+        when(studyRepository.findAllStudy(pageable)).thenReturn(responseDtos);
+        Page<StudyResponseDto> studies = studyService.getStudies(generation, field, pageable);
+
+        //then
+        verify(studyRepository, times(1)).findAllStudy(pageable);
+        assertThat(studies).hasSize(100);
+    }
+
+
 }
