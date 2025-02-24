b747ea9a676207af74b2f58c5f40797f8b6834f3
이준범 <jach1206@naver.com>
2025-02-24 21:53:51 +0900

    feat: 이메일 알림 신청 API 개발

---
+++ src/main/java/com/tave/tavewebsite/domain/emailnotification/dto/request/EmailNotificationRequeestDto.java
@@ -0,0 +1,16 @@
+package com.tave.tavewebsite.domain.emailnotification.dto.request;
+
+import com.tave.tavewebsite.domain.emailnotification.entity.EmailNotification;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record EmailNotificationRequeestDto(
+        @NotNull(message = "50자 이하로 입력해주세요.") @Size(min = 1, max = 50, message = "1~5 글자 사이로 입력해주세요.")
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "유효한 이메일 주소를 입력하세요.")
+        String email
+) {
+    public EmailNotification toEmailNotification() {
+        return EmailNotification.builder().email(this.email).build();
+    }
+}
