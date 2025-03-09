fddd21fea5d8386935e226ae8f1699427259a799
이준범 <jach1206@naver.com>
2025-03-09 15:51:59 +0900

    refactor: 신규 회원 모집 관련 엔티티 코드 수정

--- src/main/java/com/tave/tavewebsite/domain/resume/entity/Resume.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/entity/Resume.java
@@ -36,16 +36,43 @@ public class Resume {
     @Column(length = 20)
     private String major;
 
+    @Size(max = 20)
+    @Column(length = 20)
+    private String minor;
+
     @Size(min = 1, max = 8)
     @Column(length = 8)
     private String field;
+
+    @Size(min = 1, max = 5)
+    @Column(length = 5)
+    private Integer resumeGeneration;
+
+    @Size(min = 1, max = 50)
+    @Column(length = 50)
+    private String blogUrl;
+
+    @Size(min = 1, max = 50)
+    @Column(length = 50)
+    private String githubUrl;
+
+    @Size(min = 1, max = 50)
+    @Column(length = 50)
+    private String portfolioUrl;
+
+    @Size(min = 1, max = 10)
+    @Column(length = 10)
+    private String state;
 
     @ManyToOne
     @JoinColumn(name = "memberId", nullable = false)
     private Member member;
 
     @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
-    private List<Question> questions = new ArrayList<>();
+    private List<ResumeQuestion> specificResumeQuestion = new ArrayList<>();
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<ResumeQuestion> commonResumeQuestion = new ArrayList<>();
 
     @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
     private List<TimeSlot> timeSlots = new ArrayList<>();
@@ -54,16 +81,28 @@ public class Resume {
     private List<LanguageLevel> languageLevels = new ArrayList<>();
 
     @Builder
-    public Resume(String school, String major, String field, Member member) {
+    public Resume(String school, String major, String minor, Integer resumeGeneration, String blogUrl, String githubUrl,
+                  String portfolioUrl, String state, String field, Member member) {
         this.school = school;
         this.major = major;
+        this.minor = minor;
         this.field = field;
+        this.resumeGeneration = resumeGeneration;
+        this.blogUrl = blogUrl;
+        this.githubUrl = githubUrl;
+        this.portfolioUrl = portfolioUrl;
+        this.state = state;
         this.member = member;
+
         member.addResume(this);
     }
 
-    public void addQuestion(Question question) {
-        this.questions.add(question);
+    public void addSpecificQuestion(ResumeQuestion resumeQuestion) {
+        this.specificResumeQuestion.add(resumeQuestion);
+    }
+
+    public void addCommonQuestion(ResumeQuestion resumeQuestion) {
+        this.commonResumeQuestion.add(resumeQuestion);
     }
 
     public void addTimeSlot(TimeSlot timeSlot) {
