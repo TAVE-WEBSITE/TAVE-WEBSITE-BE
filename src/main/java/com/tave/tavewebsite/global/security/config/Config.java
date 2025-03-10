db96d811a03b369275715d740788559f937103f2
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-03-10 21:23:02 +0900

    hotfix: cors 설정 변경

--- src/main/java/com/tave/tavewebsite/global/security/config/Config.java
+++ src/main/java/com/tave/tavewebsite/global/security/config/Config.java
@@ -35,7 +35,7 @@ public class Config {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
         corsConfiguration.setAllowedOriginPatterns(
-                List.of("http://localhost:3000", "http://localhost:8080", "https://www.tave-wave.com"));
+                List.of("http://localhost:3000", "https://localhost:3000", "http://localhost:8080", "https://www.tave-wave.com"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         corsConfiguration.setExposedHeaders(List.of("Set-Cookie"));
