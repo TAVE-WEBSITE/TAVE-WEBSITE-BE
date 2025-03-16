1c7e26619f2a282eecb94448011906422c89a3bf
wjd4204 <brian876373@gmail.com>
2025-03-16 21:28:15 +0900

    feat: 면접 시간 조회에 대한 Exception 구현

--- src/main/java/com/tave/tavewebsite/domain/resume/exception/ErrorMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/exception/ErrorMessage.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorMessage {
 
+    UNAUTHORIZED_RESUME(403, "해당 이력서를 수정할 권한이 없습니다."),
     NOT_VALID_TIME(400, "올바르지 않은 형식의 시간대입니다."),
     NOT_FOUND_RESUME(404, "해당 이력서가 존재하지 않습니다.");
 
