fbb03ad5e39df068bee6911f829c3649422622a5
junbeom <jach1206@naver.com>
2025-03-19 20:36:23 +0900

    feat: 성공 메세지 추가

---
+++ src/main/java/com/tave/tavewebsite/domain/programinglaunguage/controller/ProgramingLanguageSuccessMessage.java
@@ -0,0 +1,15 @@
+package com.tave.tavewebsite.domain.programinglaunguage.controller;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum ProgramingLanguageSuccessMessage {
+    UPDATE_SUCCESS("레벨 수정에 성공했습니다.");
+
+    private final String message;
+
+    public String getMessage() {
+        return message;
+    }
+}
+
