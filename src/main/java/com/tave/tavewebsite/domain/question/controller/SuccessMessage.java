db9263efbfc24e6e1a42d573c45829a0f8fd9731
koreaioi <koreaioi@gachon.ac.kr>
2025-03-20 17:52:31 +0900

    feat: CRUD 기능 구현

---
+++ src/main/java/com/tave/tavewebsite/domain/question/controller/SuccessMessage.java
@@ -0,0 +1,18 @@
+package com.tave.tavewebsite.domain.question.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessMessage {
+
+    QUESTION_CREATED("지원서 질문을 성공적으로 생성했습니다."),
+    QUESTION_UPDATED("지원서 질문을 수정했습니다."),
+    QUESTION_DELETED("지원서 질문을 삭제했습니다."),
+    QUESTION_FIELD_LIST("특정 분야 지원서 질문 목록을 반환합니다."),
+    QUESTION_ALL_LIST("모든 지원서 질문 목록을 반환합니다.");
+
+    private final String message;
+
+}
