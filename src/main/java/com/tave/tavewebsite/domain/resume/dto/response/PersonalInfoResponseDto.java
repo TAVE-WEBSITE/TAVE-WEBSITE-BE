417c12b6303f597c4215b052cfc2f5190bf9b5fb
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2025-03-23 00:02:57 +0900

    fix: PersonalInfoResponseDto에 Resume 매핑 로직 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/dto/response/PersonalInfoResponseDto.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/dto/response/PersonalInfoResponseDto.java
@@ -1,19 +1,13 @@
 package com.tave.tavewebsite.domain.resume.dto.response;
 
-import com.tave.tavewebsite.domain.resume.entity.Resume;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class PersonalInfoResponseDto {
     private final String school;
     private final String major;
     private final String minor;
     private final String field;
-
-    public PersonalInfoResponseDto(Resume resume) {
-        this.school = resume.getSchool();
-        this.major = resume.getMajor();
-        this.minor = resume.getMinor();
-        this.field = resume.getField();
-    }
 }
