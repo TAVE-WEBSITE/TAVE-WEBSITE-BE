87884b3319c63296f8d63266abcbd295ed0ca380
wjd4204 <brian876373@gmail.com>
2025-03-23 14:52:38 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/main/java/com/tave/tavewebsite/domain/resume/repository/ResumeRepository.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/repository/ResumeRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
 
     Optional<Resume> findByMember(Member member);
+
     @Query("select r from Resume r join fetch r.languageLevels join fetch r.member where r.id = :id")
     Resume findResumeWithLanguageLevels(@Param("id") Long id);
 
