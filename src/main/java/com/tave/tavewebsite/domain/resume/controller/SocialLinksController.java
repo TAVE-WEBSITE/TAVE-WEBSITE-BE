66a73bf6c8704a725cc7873197a58efcbe6b5232
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2025-03-31 23:49:55 +0900

    del: 불필요한 import

--- src/main/java/com/tave/tavewebsite/domain/resume/controller/SocialLinksController.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/controller/SocialLinksController.java
@@ -8,7 +8,6 @@ import com.tave.tavewebsite.global.success.SuccessResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.net.URL;
 
 @RestController
