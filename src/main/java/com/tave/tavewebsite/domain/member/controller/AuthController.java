89e615083852470999c79371ef74222a40060434
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2025-04-13 22:58:47 +0900

    fix: api 엔드포인트 수정

--- src/main/java/com/tave/tavewebsite/domain/member/controller/AuthController.java
+++ src/main/java/com/tave/tavewebsite/domain/member/controller/AuthController.java
@@ -17,64 +17,64 @@ import static com.tave.tavewebsite.domain.member.controller.MemberSuccessMessage
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/v1/auth")
+@RequestMapping("/v1")
 public class AuthController {
 
     private final MemberService memberService;
     private final AuthService authService;
 
-    @PostMapping("/signup")
+    @PostMapping("/auth/signup")
     public SuccessResponse<MailResponseDto> registerManager(@RequestBody @Valid RegisterManagerRequestDto requestDto) {
         MailResponseDto response = memberService.saveMember(requestDto);
         return new SuccessResponse<>(response);
     }
 
-    @PostMapping("/normal/signup")
+    @PostMapping("/auth/normal/signup")
     public SuccessResponse registerMember(@RequestBody @Valid RegisterMemberRequestDto dto) {
         memberService.saveNormalMember(dto);
 
         return SuccessResponse.ok(NORMAL_MEMBER_SIGNUP.getMessage());
     }
 
-    @PostMapping("/signin")
+    @PostMapping("/auth/signin")
     public SuccessResponse<SignInResponseDto> signIn(@RequestBody SignUpRequestDto requestDto,
                                                      HttpServletResponse response) {
         SignInResponseDto signInResponseDto = authService.signIn(requestDto, response);
         return new SuccessResponse<>(signInResponseDto);
     }
 
-    @PostMapping("/refresh")
+    @PostMapping("/auth/refresh")
     public SuccessResponse<RefreshResponseDto> refreshToken(@RequestBody RefreshTokenRequestDto requestDto,
                                                             @CookieValue("refreshToken") String refreshToken,
                                                             HttpServletResponse response) {
         return new SuccessResponse<>(authService.reissueToken(requestDto, refreshToken, response));
     }
 
-    @GetMapping("/signout")
+    @GetMapping("/auth/signout")
     public SuccessResponse signOut(@RequestHeader("Authorization") String token, HttpServletResponse response) {
         authService.singOut(token, response);
         return SuccessResponse.ok(MemberSuccessMessage.SIGN_OUT_SUCCESS.getMessage());
     }
 
-    @DeleteMapping("/delete/{memberId}")
+    @DeleteMapping("/auth/delete/{memberId}")
     public SuccessResponse deleteMember(@PathVariable("memberId") Long memberId) {
         memberService.deleteMember(memberId);
         return SuccessResponse.ok(MemberSuccessMessage.DELETE_MEMBER_SUCCESS.getMessage());
     }
 
-    @PostMapping("/reset/verify")
+    @PostMapping("/normal/reset/verify")
     public SuccessResponse sendPasswordResetCode(@RequestBody ResetPasswordVerifyRequestDto requestDto) {
         memberService.verifyNormalMemberForPasswordReset(requestDto);
         return SuccessResponse.ok(MemberSuccessMessage.SEND_AUTHENTICATION_CODE.getMessage());
     }
 
-    @PostMapping("/reset/verify/code")
+    @PostMapping("/normal/reset/verify/code")
     public SuccessResponse verifyResetCode(@RequestParam String email, @RequestParam String code) {
         memberService.verifyAuthCodeForPasswordReset(email, code);
         return SuccessResponse.ok(MemberSuccessMessage.VERIFY_SUCCESS.getMessage());
     }
 
-    @PostMapping("/reset/password")
+    @PostMapping("/normal/password/change")
     public SuccessResponse resetNormalPassword(@RequestBody @Valid ResetNormalPasswordRequestDto requestDto) {
         memberService.resetNormalMemberPassword(requestDto);
         return SuccessResponse.ok(MemberSuccessMessage.RESET_PASSWORD.getMessage());
