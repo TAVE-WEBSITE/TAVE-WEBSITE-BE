c042290375475cc679970050e27bfaf1bab9504a
koreaioi <koreaioi@gachon.ac.kr>
2025-04-14 18:57:38 +0900

    feat: ResumeId로 ResumeQuestion 조회하는 Jpa메서드

--- src/main/java/com/tave/tavewebsite/domain/resume/repository/ResumeQuestionRepository.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/repository/ResumeQuestionRepository.java
@@ -1,10 +1,13 @@
 package com.tave.tavewebsite.domain.resume.repository;
 
+import com.tave.tavewebsite.domain.resume.entity.Resume;
 import com.tave.tavewebsite.domain.resume.entity.ResumeQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ResumeQuestionRepository extends JpaRepository<ResumeQuestion, Long> {
-
+    List<ResumeQuestion> findByResumeId(Long resumeId);
 }
