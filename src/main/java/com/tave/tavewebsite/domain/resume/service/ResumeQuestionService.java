aa27d41ee7573e2142dccf63b1e02cb853eca2b1
koreaioi <koreaioi@gachon.ac.kr>
2025-04-14 19:01:24 +0900

    feat: ResumeQuestion 답변 수정 비지니스 로직 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/service/ResumeQuestionService.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/service/ResumeQuestionService.java
@@ -45,6 +45,32 @@ public class ResumeQuestionService {
         return resumeQuestionList;
     }
 
+    // 공통 질문 수정
+    @Transactional
+    public void updateCommonResumeQuestion(Resume resume, List<ResumeQuestionUpdateRequest> updateRequestList) {
+
+        List<ResumeQuestion> commonResumeQuestionList = resume.getCommonResumeQuestion();
+
+        for(int i = 0 ; i < commonResumeQuestionList.size() ; i++) {
+            ResumeQuestion target = commonResumeQuestionList.get(i);
+            String answer = updateRequestList.get(i).answer();
+            target.updateAnswer(answer);
+        }
+    }
+
+    // 특정 분야 질문 수정
+    @Transactional
+    public void updateSpecificResumeQuestion(Resume resume, List<ResumeQuestionUpdateRequest> updateRequestList) {
+
+        List<ResumeQuestion> specificResumeQuestionList = resume.getSpecificResumeQuestion();
+
+        for(int i = 0 ; i < specificResumeQuestionList.size() ; i++) {
+            ResumeQuestion target = specificResumeQuestionList.get(i);
+            String answer = updateRequestList.get(i).answer();
+            target.updateAnswer(answer);
+        }
+    }
+
     public List<ResumeQuestion> findResumeQuestionsByResumeId(Long resumeId) {
         List<ResumeQuestion> resumeQuestionList = resumeQuestionRepository.findByResumeId(resumeId);
 
