9b6efa8b622c8da169f3642667388b25d26766b2
koreaioi <koreaioi@gachon.ac.kr>
2025-04-14 18:08:51 +0900

    feat: ResumeQuestion builder

--- src/main/java/com/tave/tavewebsite/domain/resume/entity/ResumeQuestion.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/entity/ResumeQuestion.java
@@ -1,29 +1,31 @@
 package com.tave.tavewebsite.domain.resume.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.tave.tavewebsite.domain.question.entity.Question;
+import com.tave.tavewebsite.global.common.BaseEntity;
+import com.tave.tavewebsite.global.common.FieldType;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import net.bytebuddy.implementation.bind.annotation.Super;
 
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class ResumeQuestion {
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ResumeQuestion extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long questionId;
+    // todo questionId가 필요한 지 의논
+    // 어차피 질문 내용이 모집 중에 수정될 일 X -> 굳이 수정이 반영되도록 해야하는가?
+//    private Long questionId;
 
     @NotNull
     @Size(min = 1, max = 300)
@@ -42,20 +44,23 @@ public class ResumeQuestion {
     @JoinColumn(name = "resume_id", nullable = false)
     private Resume resume;
 
-    @Builder
-    public ResumeQuestion(Long questionId, String question, String answer, Integer ordered, Resume resume,
-                          boolean specific) {
-        this.questionId = questionId;
-        this.question = question;
-        this.answer = answer;
-        this.ordered = ordered;
-        this.resume = resume;
+    @Enumerated(EnumType.STRING)
+    private FieldType fieldType;
 
-        // 분야별 질문이면 분야별 질문 리스트에 추가 아니면 공통질문에 추가
-        if (specific) {
-            resume.getSpecificResumeQuestion().add(this);
-        } else {
-            resume.getCommonResumeQuestion().add(this);
-        }
+    // ResumeQuestion Builder
+    public static ResumeQuestion of(Resume resume, Question question, FieldType fieldType) {
+
+        // ResumeQuestion Builder
+        ResumeQuestion resumeQuestion = ResumeQuestion.builder()
+                .resume(resume)
+                .question(question.getContent())
+                .fieldType(fieldType)
+                .ordered(question.getOrdered()) // ResumeQuestion의 order는 question을 따르는가? or 자체적인 0~5인가?
+                .build();
+
+        // Resume 측에도 resumeQuestion 설정
+        resume.addCommonQuestion(resumeQuestion);
+
+        return resumeQuestion;
     }
 }
