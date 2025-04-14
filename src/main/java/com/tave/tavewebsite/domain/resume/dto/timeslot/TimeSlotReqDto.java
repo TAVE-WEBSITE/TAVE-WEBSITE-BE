8fb54b5b826c24d8f333fc942f51a015383a1839
wjdrhs3473 <110841041+wjd4204@users.noreply.github.com>
2025-04-14 17:21:00 +0900

    fix: 시간표 DTO에 대한 유효성 검증 어노테이션 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/dto/timeslot/TimeSlotReqDto.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/dto/timeslot/TimeSlotReqDto.java
@@ -1,10 +1,14 @@
 package com.tave.tavewebsite.domain.resume.dto.timeslot;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record TimeSlotReqDto(
+        @NotNull
+        @Future
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime time
 ) {
