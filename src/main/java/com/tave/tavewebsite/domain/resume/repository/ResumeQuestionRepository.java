6afe8c3afb6b00a4592588cb0877d72c31a3274c
koreaioi <koreaioi@gachon.ac.kr>
2025-04-15 18:30:33 +0900

    feat: Resume과 FieldType으로 ResumeQuestion 조회하는 Jpa메서드 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/repository/ResumeQuestionRepository.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/repository/ResumeQuestionRepository.java
@@ -1,6 +1,8 @@
 package com.tave.tavewebsite.domain.resume.repository;
 
+import com.tave.tavewebsite.domain.resume.entity.Resume;
 import com.tave.tavewebsite.domain.resume.entity.ResumeQuestion;
+import com.tave.tavewebsite.global.common.FieldType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface ResumeQuestionRepository extends JpaRepository<ResumeQuestion, Long> {
     List<ResumeQuestion> findByResumeId(Long resumeId);
+
+    // todo N+1 문제 해결을 위해서 Fetch Join!
+    List<ResumeQuestion> findByResumeAndFieldType(Resume resume, FieldType fieldType);
 }
