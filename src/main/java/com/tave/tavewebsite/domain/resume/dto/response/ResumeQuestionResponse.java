c18e2226c0fb793f9d160ba309dacce41a5f68de
koreaioi <koreaioi@gachon.ac.kr>
2025-04-16 00:14:47 +0900

    feat: ResumeQuestionResponse 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/dto/response/ResumeQuestionResponse.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/dto/response/ResumeQuestionResponse.java
@@ -1,26 +1,18 @@
 package com.tave.tavewebsite.domain.resume.dto.response;
 
-import com.tave.tavewebsite.domain.resume.entity.ResumeQuestion;
-import com.tave.tavewebsite.global.common.FieldType;
-import lombok.Builder;
+import java.util.List;
 
-@Builder
+
 public record ResumeQuestionResponse(
 
-        Long id,
-        String question,
-        String answer,
-        FieldType fieldType,
-        Integer ordered
+        List<DetailResumeQuestionResponse> common,
+        List<DetailResumeQuestionResponse> specific
 
 ) {
-    public static ResumeQuestionResponse from(ResumeQuestion resumeQuestion) {
-        return ResumeQuestionResponse.builder()
-                .id(resumeQuestion.getId())
-                .question(resumeQuestion.getQuestion())
-                .answer(resumeQuestion.getAnswer())
-                .fieldType(resumeQuestion.getFieldType())
-                .ordered(resumeQuestion.getOrdered())
-                .build();
+    public static ResumeQuestionResponse of(
+            List<DetailResumeQuestionResponse> common,
+            List<DetailResumeQuestionResponse> specific
+    ) {
+        return new ResumeQuestionResponse(common, specific);
     }
 }
