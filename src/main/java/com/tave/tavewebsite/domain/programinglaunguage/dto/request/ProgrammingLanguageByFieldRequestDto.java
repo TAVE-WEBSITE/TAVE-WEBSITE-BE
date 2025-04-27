e952fa15a97160ca9f6ea339763357bd91100eed
junbeom <jach1206@naver.com>
2025-04-27 20:32:46 +0900

    feat: 프로그래밍 언어 컨트롤러 개발에 필요한 dto 생성

---
+++ src/main/java/com/tave/tavewebsite/domain/programinglaunguage/dto/request/ProgrammingLanguageByFieldRequestDto.java
@@ -0,0 +1,6 @@
+package com.tave.tavewebsite.domain.programinglaunguage.dto.request;
+
+public record ProgrammingLanguageByFieldRequestDto(
+        String field
+) {
+}
---
+++ src/main/java/com/tave/tavewebsite/domain/programinglaunguage/dto/response/ProgrammingLanguageResponseDto.java
@@ -0,0 +1,8 @@
+package com.tave.tavewebsite.domain.programinglaunguage.dto.response;
+
+public record ProgrammingLanguageResponseDto(
+        Long id,
+        String field,
+        String language
+) {
+}
