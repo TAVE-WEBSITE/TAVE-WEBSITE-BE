7b34ef9c0eec58c222c3c4128506bbc9a292cc11
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2025-04-28 00:17:04 +0900

    fix: Resume field 접근 시 FieldType 사용하도록 수정

--- src/main/java/com/tave/tavewebsite/domain/resume/service/PersonalInfoService.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/service/PersonalInfoService.java
@@ -16,7 +16,9 @@ import com.tave.tavewebsite.domain.resume.mapper.ResumeMapper;
 import com.tave.tavewebsite.domain.resume.repository.ResumeRepository;
 import com.tave.tavewebsite.global.common.FieldType;
 import jakarta.transaction.Transactional;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -34,14 +36,10 @@ public class PersonalInfoService {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(MemberNotFoundException::new);
 
-        // 지원 분야 값 검증 및 변환
-        Resume.FieldType fieldType = validateAndConvertFieldType(requestDto.getField());
         Resume savedResume = resumeRepository.save(ResumeMapper.toResume(requestDto, member));
 
         // 필드값 기준으로 질문들을 찾은 후 해당 질문들을 모두 저장시키는 과정
-        List<ProgramingLanguage> byField = programingLanguageRepository.findByField(
-                FieldType.valueOf(
-                        savedResume.getField())); //todo *****지우야**** 여기 지금 resume field가 String으로 돼있어서 내가 강제로 바꿔놨는데 field 타입 바꾸고 나서 여기 코드도 수정해줘
+        List<ProgramingLanguage> byField = programingLanguageRepository.findByField(savedResume.getField());
         List<LanguageLevel> languageLevel = LanguageLevelMapper.toLanguageLevel(byField, savedResume);
 
         languageLevelRepository.saveAll(languageLevel);
@@ -68,10 +66,6 @@ public class PersonalInfoService {
     public void updatePersonalInfo(Long resumeId, PersonalInfoRequestDto requestDto) {
         Resume resume = resumeRepository.findById(resumeId)
                 .orElseThrow(ResumeNotFoundException::new);
-
-        // 지원 분야 값 검증 및 변환
-        Resume.FieldType fieldType = validateAndConvertFieldType(requestDto.getField());
-
         resume.updatePersonalInfo(requestDto);
     }
 
@@ -81,14 +75,5 @@ public class PersonalInfoService {
                 .orElseThrow(ResumeNotFoundException::new);
 
         resumeRepository.delete(resume);
-    }
-
-    // 지원 분야 값 검증 및 변환 메서드
-    private Resume.FieldType validateAndConvertFieldType(String field) {
-        try {
-            return Resume.FieldType.valueOf(field.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new ResumeNotFoundException();
-        }
     }
 }
