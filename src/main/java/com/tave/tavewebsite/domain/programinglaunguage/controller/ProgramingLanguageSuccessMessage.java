1577596a19a33bc3503fead8addf28f84a95d1f1
junbeom <jach1206@naver.com>
2025-04-27 20:33:07 +0900

    feat: 프로그래밍 언어 컨트롤러 개발에 필요한 성공 메세지 및 실패 메세지 추가

--- src/main/java/com/tave/tavewebsite/domain/programinglaunguage/controller/ProgramingLanguageSuccessMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/programinglaunguage/controller/ProgramingLanguageSuccessMessage.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum ProgramingLanguageSuccessMessage {
     UPDATE_SUCCESS("레벨 수정에 성공했습니다."),
-    READ_SUCCESS("레벨 조회에 성공했습니다.");
+    READ_LEVEL_SUCCESS("레벨 조회에 성공했습니다."),
+    READ_LANGUAGE_SUCCESS("모든 언어 조회에 성공했습니다."),
+    CREATE_LANGUAGE_SUCCESS("언어 생성에 성공했습니다."),
+    DELETE_LANGUAGE_SUCCESS("언어 삭제에 성공했습니다.");
 
     private final String message;
 
---
+++ src/main/java/com/tave/tavewebsite/domain/programinglaunguage/exception/ProgrammingLanguageErrorMessage.java
@@ -0,0 +1,15 @@
+package com.tave.tavewebsite.domain.programinglaunguage.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ProgrammingLanguageErrorMessage {
+
+    NOT_FOUND_PROGRAMMING_LANGUAGE(400, "해당 언어를 찾을 수 없습니다.");
+
+    final int code;
+    final String message;
+
+}
