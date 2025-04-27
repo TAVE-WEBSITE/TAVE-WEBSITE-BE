9c8084af9781be1cacf8ec225d7de40abbadbc8c
junbeom <jach1206@naver.com>
2025-04-27 20:58:58 +0900

    chore: field에 대한 에러 메시지 수정

--- src/main/java/com/tave/tavewebsite/domain/programinglaunguage/exception/ProgrammingLanguageErrorMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/programinglaunguage/exception/ProgrammingLanguageErrorMessage.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ProgrammingLanguageErrorMessage {
 
     NOT_FOUND_PROGRAMMING_LANGUAGE(400, "해당 언어를 찾을 수 없습니다."),
-    NOT_FOUND_FIELD_LANGUAGE(400, "해당 분야를 찾을 수 없습니다.");
+    NOT_FOUND_FIELD_LANGUAGE(400, "해당 언어를 찾을 수 없습니다.");
 
 
     final int code;
