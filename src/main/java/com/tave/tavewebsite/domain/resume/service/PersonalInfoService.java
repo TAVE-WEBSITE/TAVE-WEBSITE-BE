aad0f3c41ce93a5a05226ef1efb92f6795770e6d
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2025-04-28 13:21:14 +0900

    refactor: validateAndConvertFieldType() 메서드 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/service/PersonalInfoService.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/service/PersonalInfoService.java
@@ -10,6 +10,7 @@ import com.tave.tavewebsite.domain.programinglaunguage.util.LanguageLevelMapper;
 import com.tave.tavewebsite.domain.resume.dto.request.PersonalInfoRequestDto;
 import com.tave.tavewebsite.domain.resume.dto.response.PersonalInfoResponseDto;
 import com.tave.tavewebsite.domain.resume.entity.Resume;
+import com.tave.tavewebsite.domain.resume.exception.FieldTypeInvalidException;
 import com.tave.tavewebsite.domain.resume.exception.MemberNotFoundException;
 import com.tave.tavewebsite.domain.resume.exception.ResumeNotFoundException;
 import com.tave.tavewebsite.domain.resume.mapper.ResumeMapper;
@@ -36,7 +37,8 @@ public class PersonalInfoService {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(MemberNotFoundException::new);
 
-        Resume savedResume = resumeRepository.save(ResumeMapper.toResume(requestDto, member));
+        FieldType fieldType = validateAndConvertFieldType(requestDto.getField());
+        Resume savedResume = resumeRepository.save(ResumeMapper.toResume(requestDto, member, fieldType));
 
         // 필드값 기준으로 질문들을 찾은 후 해당 질문들을 모두 저장시키는 과정
         List<ProgramingLanguage> byField = programingLanguageRepository.findByField(savedResume.getField());
@@ -51,8 +53,9 @@ public class PersonalInfoService {
         Resume resume = resumeRepository.findByMemberId(memberId)
                 .orElseThrow(ResumeNotFoundException::new);
 
+        FieldType fieldType = validateAndConvertFieldType(requestDto.getField());
         // 기존 정보 갱신 (임시 저장 위해)
-        resume.updatePersonalInfo(requestDto);
+        resume.updatePersonalInfo(requestDto, fieldType);
     }
 
     public PersonalInfoResponseDto getPersonalInfo(Long resumeId) {
@@ -66,7 +69,9 @@ public class PersonalInfoService {
     public void updatePersonalInfo(Long resumeId, PersonalInfoRequestDto requestDto) {
         Resume resume = resumeRepository.findById(resumeId)
                 .orElseThrow(ResumeNotFoundException::new);
-        resume.updatePersonalInfo(requestDto);
+        FieldType fieldType = validateAndConvertFieldType(requestDto.getField());
+
+        resume.updatePersonalInfo(requestDto, fieldType);
     }
 
     @Transactional
@@ -75,5 +80,13 @@ public class PersonalInfoService {
                 .orElseThrow(ResumeNotFoundException::new);
 
         resumeRepository.delete(resume);
+    }
+
+    private FieldType validateAndConvertFieldType(String field) {
+        try {
+            return FieldType.valueOf(field.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new FieldTypeInvalidException();
+        }
     }
 }
