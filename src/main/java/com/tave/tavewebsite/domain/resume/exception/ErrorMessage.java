43c61841a0463f31d5c8ebf5d1c59f30e67fea61
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2025-04-28 13:19:29 +0900

    feat: 에러메시지 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/exception/ErrorMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/exception/ErrorMessage.java
@@ -15,7 +15,10 @@ public enum ErrorMessage {
     UNAUTHORIZED_RESUME(403, "해당 이력서를 수정할 권한이 없습니다."),
     NOT_VALID_TIME(400, "올바르지 않은 형식의 시간대입니다."),
 
-    SOCIAL_LINK_NOT_FOUND(404, "url이 존재하지 않습니다.");
+    SOCIAL_LINK_NOT_FOUND(404, "url이 존재하지 않습니다."),
+
+    FIELD_TYPE_INVALID(400, "유효하지 않은 필드 타입입니다.");
+
     private final int code;
     private final String message;
 }
