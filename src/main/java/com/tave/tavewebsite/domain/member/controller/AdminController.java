0aaae309a3ff33aa04ff0b82583ba47c4ef386fd
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2025-05-03 22:32:04 +0900

    refactor: 승인 대기/완료 운영진 조회 API POST 방식으로 분리

--- src/main/java/com/tave/tavewebsite/domain/member/controller/AdminController.java
+++ src/main/java/com/tave/tavewebsite/domain/member/controller/AdminController.java
@@ -5,15 +5,10 @@ import com.tave.tavewebsite.domain.member.service.AdminService;
 import com.tave.tavewebsite.global.success.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/admin")
@@ -22,12 +17,26 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    @GetMapping("/manager")
-    public SuccessResponse<Page<ManagerResponseDto>> getManagers(
-            @RequestParam(defaultValue = "ALL", name = "status") String status,
-            @PageableDefault(size = 8) Pageable pageable) {
-        return adminService.getManagersByStatus(status, pageable);
+    // 승인 완료 운영진 조회
+    @PostMapping("/manager/authorized")
+    public SuccessResponse<Page<ManagerResponseDto>> getAuthorizedManagers(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "8") int size) {
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return adminService.getManagersByStatus("AUTHORIZED", pageable);
     }
+
+    // 승인 대기 운영진 조회
+    @PostMapping("/manager/unauthorized")
+    public SuccessResponse<Page<ManagerResponseDto>> getUnauthorizedManagers(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "8") int size) {
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return adminService.getManagersByStatus("UNAUTHORIZED", pageable);
+    }
+
 
     @DeleteMapping("/{memberId}")
     public SuccessResponse deleteManager(@PathVariable Long memberId) {
