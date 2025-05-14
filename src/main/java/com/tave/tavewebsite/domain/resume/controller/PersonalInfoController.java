de535882c99f7248089f47705259f09176cb12d2
jiwoo <1114jiwoo@gmail.com>
2025-05-15 01:21:43 +0900

    feat: 개인정보 임시저장 controller

--- src/main/java/com/tave/tavewebsite/domain/resume/controller/PersonalInfoController.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/controller/PersonalInfoController.java
@@ -1,9 +1,8 @@
 package com.tave.tavewebsite.domain.resume.controller;
 
 import com.tave.tavewebsite.domain.resume.dto.request.PersonalInfoRequestDto;
-import com.tave.tavewebsite.domain.resume.dto.response.CreatePersonalInfoResponse;
+import com.tave.tavewebsite.domain.resume.dto.request.TempPersonalInfoDto;
 import com.tave.tavewebsite.domain.resume.dto.response.PersonalInfoResponseDto;
-import com.tave.tavewebsite.domain.resume.dto.response.ResumeQuestionResponse;
 import com.tave.tavewebsite.domain.resume.service.PersonalInfoService;
 import com.tave.tavewebsite.global.success.SuccessResponse;
 import jakarta.validation.Valid;
@@ -11,10 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import static com.tave.tavewebsite.domain.resume.controller.PersonalInfoSuccessMessage.*;
+
 @RestController
 @RequiredArgsConstructor
 @Validated
-@RequestMapping("/v1/member/info")
+@RequestMapping("/v1/normal/info")
 public class PersonalInfoController {
 
     private final PersonalInfoService personalInfoService;
@@ -24,14 +25,14 @@ public class PersonalInfoController {
     public SuccessResponse createPersonalInfo(@PathVariable("memberId") Long memberId,
                                               @RequestBody @Valid PersonalInfoRequestDto requestDto) {
         personalInfoService.createPersonalInfo(memberId, requestDto);
-        return SuccessResponse.ok(PersonalInfoSuccessMessage.CREATE_SUCCESS.getMessage());
+        return SuccessResponse.ok(CREATE_SUCCESS.getMessage());
     }
 
     // 개인정보 조회
     @GetMapping("/{resumeId}")
     public SuccessResponse<PersonalInfoResponseDto> getPersonalInfo(@PathVariable("resumeId") Long resumeId) {
         return new SuccessResponse<>(personalInfoService.getPersonalInfo(resumeId),
-                PersonalInfoSuccessMessage.READ_SUCCESS.getMessage());
+                READ_SUCCESS.getMessage());
     }
 
     // 개인정보 수정
@@ -39,21 +40,29 @@ public class PersonalInfoController {
     public SuccessResponse updatePersonalInfo(@PathVariable("resumeId") Long resumeId,
                                               @RequestBody @Valid PersonalInfoRequestDto requestDto) {
         personalInfoService.updatePersonalInfo(resumeId, requestDto);
-        return SuccessResponse.ok(PersonalInfoSuccessMessage.UPDATE_SUCCESS.getMessage());
+        return SuccessResponse.ok(UPDATE_SUCCESS.getMessage());
     }
 
     // 개인정보 삭제
     @DeleteMapping("/{resumeId}")
     public SuccessResponse deletePersonalInfo(@PathVariable("resumeId") Long resumeId) {
         personalInfoService.deletePersonalInfo(resumeId);
-        return SuccessResponse.ok(PersonalInfoSuccessMessage.DELETE_SUCCESS.getMessage());
+        return SuccessResponse.ok(DELETE_SUCCESS.getMessage());
     }
 
-    // 임시 저장
+    // 개인정보 임시 저장
     @PostMapping("/temp-save/{memberId}")
     public SuccessResponse tempSavePersonalInfo(@PathVariable("memberId") Long memberId,
                                                 @RequestBody @Valid PersonalInfoRequestDto requestDto) {
         personalInfoService.tempSavePersonalInfo(memberId, requestDto);
-        return SuccessResponse.ok(PersonalInfoSuccessMessage.TEMP_SAVE_SUCCESS.getMessage());
+        return SuccessResponse.ok(TEMP_SAVE_SUCCESS.getMessage());
     }
+
+    // 임시 저장 개인정보 불러오기
+    @GetMapping("/temp-save/{memberId}")
+    public SuccessResponse<TempPersonalInfoDto> getTempSavedPersonalInfo(@PathVariable("memberId") Long memberId) {
+        TempPersonalInfoDto response = personalInfoService.getTempSavedPersonalInfo(memberId);
+        return new SuccessResponse<>(response, TEMP_LOAD_SUCCESS.getMessage());
+    }
+
 }
