461c35cd95065422d55ecdfeb3324d9ecf70cb4a
jiwoo <1114jiwoo@gmail.com>
2025-05-15 01:23:16 +0900

    feat: 답변 redis 통한 임시저장

---
+++ src/main/java/com/tave/tavewebsite/domain/resume/controller/ResumeAnswerTempController.java
@@ -0,0 +1,37 @@
+package com.tave.tavewebsite.domain.resume.controller;
+
+import com.tave.tavewebsite.domain.resume.dto.request.ResumeAnswerTempDto;
+import com.tave.tavewebsite.domain.resume.service.ResumeAnswerTempService;
+import com.tave.tavewebsite.global.success.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.tave.tavewebsite.domain.resume.controller.PersonalInfoSuccessMessage.TEMP_LOAD_SUCCESS;
+import static com.tave.tavewebsite.domain.resume.controller.PersonalInfoSuccessMessage.TEMP_SAVE_SUCCESS;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/normal/resume/temp-answer")
+public class ResumeAnswerTempController {
+
+    private final ResumeAnswerTempService tempService;
+
+    // 임시 저장
+    @PostMapping("/{resumeId}")
+    public SuccessResponse saveTempAnswers(@PathVariable Long resumeId,
+                                           @RequestParam int page,
+                                           @RequestBody List<ResumeAnswerTempDto> answers) {
+        tempService.tempSaveAnswers(resumeId, page, answers);
+        return SuccessResponse.ok(TEMP_SAVE_SUCCESS.getMessage());
+    }
+
+    // 임시 저장 불러오기
+    @GetMapping("/{resumeId}")
+    public SuccessResponse<List<ResumeAnswerTempDto>> getTempAnswers(@PathVariable Long resumeId,
+                                                                     @RequestParam int page) {
+        List<ResumeAnswerTempDto> response = tempService.getTempSavedAnswers(resumeId, page);
+        return new SuccessResponse<>(response, TEMP_LOAD_SUCCESS.getMessage());
+    }
+}
