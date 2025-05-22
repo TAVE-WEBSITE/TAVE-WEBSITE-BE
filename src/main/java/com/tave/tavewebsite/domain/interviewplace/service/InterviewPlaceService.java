c5dbdf3b458ca4686a0f631795e8b1cc0b794406
koreaioi <koreaioi@gachon.ac.kr>
2025-05-23 01:37:25 +0900

    feat: InterviewPlaceService 추가

---
+++ src/main/java/com/tave/tavewebsite/domain/interviewplace/service/InterviewPlaceService.java
@@ -0,0 +1,23 @@
+package com.tave.tavewebsite.domain.interviewplace.service;
+
+import com.tave.tavewebsite.domain.interviewplace.dto.request.InterviewPlaceSaveDto;
+import com.tave.tavewebsite.domain.interviewplace.dto.response.InterviewPlaceResponse;
+import com.tave.tavewebsite.domain.interviewplace.entity.InterviewPlace;
+import com.tave.tavewebsite.domain.interviewplace.repository.InterviewPlaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InterviewPlaceService {
+
+    private final InterviewPlaceRepository interviewPlaceRepository;
+
+    public InterviewPlaceResponse saveInterviewPlace(InterviewPlaceSaveDto dto) {
+
+        InterviewPlace interviewPlace = InterviewPlace.of(dto);
+
+        return InterviewPlaceResponse.of(interviewPlaceRepository.save(interviewPlace));
+    }
+
+}
