7e4570d4ca81da4d7056e46069ffacdccad5c9e4
koreaioi <koreaioi@gachon.ac.kr>
2025-05-23 03:57:17 +0900

    feat: InterviewPlaceSaveDto 추가

---
+++ src/main/java/com/tave/tavewebsite/domain/interviewplace/dto/request/InterviewPlaceSaveDto.java
@@ -0,0 +1,17 @@
+package com.tave.tavewebsite.domain.interviewplace.dto.request;
+
+
+public record InterviewPlaceSaveDto(
+
+        String generalAddress,
+        String detailAddress,
+        String firstOpenChatLink,
+        String secondOpenChatLink,
+        String thirdOpenChatLink,
+        String fourthOpenChatLink,
+        String firstDocumentLink,
+        String secondDocumentLink,
+        String thridDocumentLink,
+        String fourthDocumentLink
+) {
+}
