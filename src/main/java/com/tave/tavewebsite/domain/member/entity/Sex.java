2dddde8401c3c8d6f5e1d422e4b937ae01c0875f
koreaioi <koreaioi@gachon.ac.kr>
2025-05-26 22:58:41 +0900

    fix: FieldType과 Sex에 displayName 필드 추가

--- src/main/java/com/tave/tavewebsite/domain/member/entity/Sex.java
+++ src/main/java/com/tave/tavewebsite/domain/member/entity/Sex.java
@@ -1,5 +1,15 @@
 package com.tave.tavewebsite.domain.member.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Sex {
-    MALE, FEMALE, NULL, NON_SEX
+    MALE("남자"),
+    FEMALE("여자"),
+    NULL("분석 실패"),
+    NON_SEX("분석 실패");
+
+    private final String displayName;
 }
