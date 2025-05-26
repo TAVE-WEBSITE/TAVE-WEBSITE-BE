bc4407358c3a2c280c3c018c06a82befbfd7c7f4
junbeom <jach1206@naver.com>
2025-05-26 09:16:05 +0900

    feat: 도메인 초기 설정 서비스 계층 코드 작성

---
+++ src/main/java/com/tave/tavewebsite/domain/apply/initial/setup/service/ApplyInitialSetupService.java
@@ -0,0 +1,52 @@
+package com.tave.tavewebsite.domain.apply.initial.setup.service;
+
+import com.tave.tavewebsite.domain.apply.initial.setup.dto.request.ApplyInitialSetupRequestDto;
+import com.tave.tavewebsite.domain.apply.initial.setup.dto.response.ApplyInitialSetupReadResponseDto;
+import com.tave.tavewebsite.domain.apply.initial.setup.entity.ApplyInitialSetup;
+import com.tave.tavewebsite.domain.apply.initial.setup.exception.ApplyInitialSetupException.ApplyInitialSetupNotFoundException;
+import com.tave.tavewebsite.domain.apply.initial.setup.repository.ApplyInitialSetupRepository;
+import com.tave.tavewebsite.domain.apply.initial.setup.util.ApplyInitialSetUpMapper;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ApplyInitialSetupService {
+
+    private final ApplyInitialSetupRepository applyInitialSetupRepository;
+
+    @Transactional(readOnly = true)
+    public ApplyInitialSetupReadResponseDto getInitialSetup() {
+        ApplyInitialSetup applyInitialSetup = applyInitialSetupRepository.findById(1L)
+                .orElseThrow(ApplyInitialSetupNotFoundException::new);
+        return ApplyInitialSetUpMapper.toApplyInitialSetupReadResponseDto(applyInitialSetup);
+    }
+
+    public void saveInitialSetup(ApplyInitialSetupRequestDto dto) {
+        if (applyInitialSetupRepository.existsById(1L)) {
+            Optional<ApplyInitialSetup> byId = applyInitialSetupRepository.findById(1L);
+            byId.get().updateFrom(dto);
+            return;
+        }
+        ApplyInitialSetup entity = ApplyInitialSetUpMapper.toEntity(dto);
+        applyInitialSetupRepository.save(entity);
+    }
+
+    public void updateInitialSetup(ApplyInitialSetupRequestDto dto) {
+        ApplyInitialSetup existing = applyInitialSetupRepository.findById(1L)
+                .orElseThrow(ApplyInitialSetupNotFoundException::new);
+
+        existing.updateFrom(dto);
+    }
+
+    public void deleteInitialSetup() {
+        if (!applyInitialSetupRepository.existsById(1L)) {
+            throw new ApplyInitialSetupNotFoundException();
+        }
+
+        applyInitialSetupRepository.deleteById(1L);
+    }
+}
