a7fa03be612361a3d48b6c6054dd924d44cc83b2
koreaioi <koreaioi@gachon.ac.kr>
2025-05-26 21:56:24 +0900

    feat: 엑셀 데이터 파싱을 위한 ExcelUtils 추가

---
+++ src/main/java/com/tave/tavewebsite/domain/interviewfinal/utils/ExcelUtils.java
@@ -0,0 +1,72 @@
+package com.tave.tavewebsite.domain.interviewfinal.utils;
+
+import com.tave.tavewebsite.domain.member.entity.Sex;
+import com.tave.tavewebsite.global.common.FieldType;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@Component
+public class ExcelUtils {
+
+    public static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("M월 d일");
+    public static final String PARSING_NULL = "분석 실패";
+    public static final String WEB_FRONTEND = "Web 프론트엔드";
+    public static final String APP_FRONTEND = "App 프론트엔드";
+    public static final String BACKEND = "백엔드";
+    public static final String DEEPLEARNING = "딥러닝";
+    public static final String DATAANALYSIST = "데이터분석";
+    public static final String DESIGN = "디자인";
+    public static final String MALE = "남자";
+    public static final String FEMALE = "여자";
+
+
+    public String getDayToCell(Cell cell) {
+        if (cell == null) return "";
+
+        Date day = cell.getDateCellValue();
+        return DATE_FORMAT.format(day);
+    }
+
+    public String getTimeToCell(Cell cell) {
+        return new DataFormatter().formatCellValue(cell);
+    }
+
+    public String getStringToCell(Cell cell) {
+        if (cell == null) return PARSING_NULL;
+        return cell.getStringCellValue().trim();
+    }
+
+    public Integer getIntegerToCell(Cell cell) {
+        Integer num = (int) cell.getNumericCellValue();
+        return num;
+    }
+
+    public Sex StringConvertToSex(String sex) {
+        return switch (sex) {
+            case PARSING_NULL -> Sex.NULL;
+            case MALE -> Sex.MALE;
+            case FEMALE -> Sex.FEMALE;
+            default -> Sex.NON_SEX;
+        };
+    }
+
+    public FieldType StringConvertToField(String field) {
+        if (field.equals(PARSING_NULL)) return FieldType.EXCEL_PARSING_NULL;
+
+        return switch (field.trim()) {
+            case BACKEND -> FieldType.BACKEND;
+            case DATAANALYSIST -> FieldType.DATAANALYSIS;
+            case DEEPLEARNING -> FieldType.DEEPLEARNING;
+            case WEB_FRONTEND -> FieldType.WEBFRONTEND;
+            case DESIGN -> FieldType.DESIGN;
+            case APP_FRONTEND -> FieldType.APPFRONTEND;
+            default -> FieldType.PARSING_NULL;
+        };
+    }
+
+}
