aac22f85bc49a18e251dc96d1f23dea831eb23dc
koreaioi <koreaioi@gachon.ac.kr>
2025-05-26 23:29:46 +0900

    feat: InterviewFinalRepository 페이지네이션 조회 JPA 메서드 추가

--- src/main/java/com/tave/tavewebsite/domain/interviewfinal/repository/InterviewFinalRepository.java
+++ src/main/java/com/tave/tavewebsite/domain/interviewfinal/repository/InterviewFinalRepository.java
@@ -1,7 +1,13 @@
 package com.tave.tavewebsite.domain.interviewfinal.repository;
 
 import com.tave.tavewebsite.domain.interviewfinal.entity.InterviewFinal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterviewFinalRepository extends JpaRepository<InterviewFinal, Integer> {
+
+    Page<InterviewFinal> findAllByOrderByInterviewDayAscInterviewTimeAscUsernameAsc(Pageable pageable);
+
+
 }
