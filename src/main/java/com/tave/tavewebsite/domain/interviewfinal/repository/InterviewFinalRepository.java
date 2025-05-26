9b19c499338bdb0aabf80eb2e36896f783ea0db5
koreaioi <koreaioi@gachon.ac.kr>
2025-05-26 21:25:21 +0900

    feat: InterviewFinalRepository 추가

---
+++ src/main/java/com/tave/tavewebsite/domain/interviewfinal/repository/InterviewFinalRepository.java
@@ -0,0 +1,7 @@
+package com.tave.tavewebsite.domain.interviewfinal.repository;
+
+import com.tave.tavewebsite.domain.interviewfinal.entity.InterviewFinal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewFinalRepository extends JpaRepository<InterviewFinal, Integer> {
+}
