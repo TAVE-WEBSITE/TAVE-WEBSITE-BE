06048e012c0f827fcd6883ab70ce37412d7bf37c
wjd4204 <brian876373@gmail.com>
2025-05-26 22:28:31 +0900

    refactor: 현재 api를 이용중인 사용자의 정보를 가져오도록 리팩터링

--- src/main/java/com/tave/tavewebsite/global/security/entity/CustomUserDetails.java
+++ src/main/java/com/tave/tavewebsite/global/security/entity/CustomUserDetails.java
@@ -1,7 +1,6 @@
 package com.tave.tavewebsite.global.security.entity;
 
 import com.tave.tavewebsite.domain.member.entity.Member;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
--- src/main/java/com/tave/tavewebsite/global/security/utils/JwtTokenProvider.java
+++ src/main/java/com/tave/tavewebsite/global/security/utils/JwtTokenProvider.java
@@ -28,8 +28,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
