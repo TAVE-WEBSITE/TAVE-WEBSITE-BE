95a739a55f4738ed30d3f731729b890cc30e77d3
koreaioi <koreaioi@gachon.ac.kr>
2025-05-28 22:42:07 +0900

    feat: Table 어노테이션 추가

--- src/main/java/com/tave/tavewebsite/domain/interviewfinal/entity/InterviewFinal.java
+++ src/main/java/com/tave/tavewebsite/domain/interviewfinal/entity/InterviewFinal.java
@@ -14,6 +14,7 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Getter
 @SuperBuilder
+@Table(name="interview_final")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InterviewFinal extends BaseEntity {
 
