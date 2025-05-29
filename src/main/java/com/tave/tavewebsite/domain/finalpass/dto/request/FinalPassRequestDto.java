0f8b507c6afc56b8773c4e9fce324f86dd987a73
jiwoo <1114jiwoo@gmail.com>
2025-05-29 15:46:19 +0900

    feat: 유효성 검증 추가

--- src/main/java/com/tave/tavewebsite/domain/finalpass/dto/request/FinalPassRequestDto.java
+++ src/main/java/com/tave/tavewebsite/domain/finalpass/dto/request/FinalPassRequestDto.java
@@ -1,5 +1,6 @@
 package com.tave.tavewebsite.domain.finalpass.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,16 +11,37 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FinalPassRequestDto {
+    @NotNull(message = "총 회비 필수로 입력해주세요.")
     private Integer totalFee;
+
     private Integer clubFee;
+
     private Integer mtFee;
+
+    @NotNull(message = "회비 납부 마감기한 필수로 입력해주세요.")
     private LocalDate feeDeadline;
+
+    @NotNull(message = "은행명 필수로 입력해주세요.")
     private String bankName;
+
+    @NotNull(message = "계좌번호 필수로 입력해주세요.")
     private String accountNumber;
+
+    @NotNull(message = "예금주 필수로 입력해주세요.")
     private String accountHolder;
+
+    @NotNull(message = "아지트 초대 설문 조사 링크 필수로 입력해주세요.")
     private String surveyLink;
+
+    @NotNull(message = "아지트 초대 설문 조사 마감기한 필수로 입력해주세요.")
     private LocalDate surveyDeadline;
+
+    @NotNull(message = "OT 공지방 링크 필수로 입력해주세요.")
     private String otLink;
+
+    @NotNull(message = "OT 공지방 비밀번호 필수로 입력해주세요.")
     private String otPassword;
+
+    @NotNull(message = "OT 마감기한 필수로 입력해주세요. ")
     private LocalDate otDeadline;
 }
