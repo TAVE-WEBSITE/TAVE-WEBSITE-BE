3b87193e1d6578876d49273e8e6aee1b198f10a0
jiwoo <1114jiwoo@gmail.com>
2025-06-05 23:37:36 +0900

    feat: 소셜주소, 포트폴리오 임시저장 로직 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/controller/SocialLinksSuccessMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/controller/SocialLinksSuccessMessage.java
@@ -8,7 +8,11 @@ public enum SocialLinksSuccessMessage {
     UPDATE_SUCCESS("url 수정에 성공했습니다."),
     READ_SUCCESS("url 조회에 성공했습니다."),
     DELETE_SUCCESS("url 삭제에 성공했습니다."),
-    UPLOAD_SUCCESS("포트폴리오 업로드에 성공했습니다.");
+    UPLOAD_SUCCESS("포트폴리오 업로드에 성공했습니다."),
+    TEMP_SAVE_SUCCESS("소셜 링크 임시 저장에 성공했습니다."),
+    TEMP_READ_SUCCESS("소셜 링크 임시 조회에 성공했습니다."),
+    PORTFOLIO_TEMP_SAVE_SUCCESS("포트폴리오 임시 저장에 성공했습니다."),
+    PORTFOLIO_TEMP_READ_SUCCESS("포트폴리오 임시 조회에 성공했습니다.");
 
     private final String message;
 
