51039c5b5ec8e425c4370fa6e5e5601a500681b5
koreaioi <koreaioi@gachon.ac.kr>
2025-06-08 20:19:34 +0900

    feat: MemberId와 기수로 InterviewFinal 조회

--- src/main/java/com/tave/tavewebsite/domain/interviewfinal/repository/InterviewFinalRepository.java
+++ src/main/java/com/tave/tavewebsite/domain/interviewfinal/repository/InterviewFinalRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InterviewFinalRepository extends JpaRepository<InterviewFinal, Integer> {
 
     Page<InterviewFinal> findAllByOrderByInterviewDateAscInterviewTimeAscUsernameAsc(Pageable pageable);
 
     List<InterviewFinal> findAllByOrderByInterviewDateAscInterviewTimeAscUsernameAsc();
+
+    Optional<InterviewFinal> findByMemberIdAndGeneration(Long memberId, Integer generation);
 
 }
