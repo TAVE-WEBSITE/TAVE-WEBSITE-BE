88df73d4eaf1c91f3bd9e64b7732755b0f091298
koreaioi <koreaioi@gachon.ac.kr>
2025-06-15 21:51:07 +0900

    feat: 그룹화된 Map 컬렉션을 Dto로 변환하는 GroupUtil 로직

--- src/main/java/com/tave/tavewebsite/domain/interviewfinal/utils/InterviewGroupUtil.java
+++ src/main/java/com/tave/tavewebsite/domain/interviewfinal/utils/InterviewGroupUtil.java
@@ -24,4 +24,52 @@ public class InterviewGroupUtil {
                 ));
     }
 
+    public List<InterviewTimeTableGroupByDayDto> createTimeTableDtoList(
+            Map<LocalDate, Map<LocalTime, List<InterviewFinal>>>  group
+    ) {
+
+        List<InterviewTimeTableGroupByDayDto> groupByDayList = new ArrayList<>();
+
+        // GroupByDate 로 순회
+        for(LocalDate date : getGroupKeyByDate(group)){
+            Map<LocalTime, List<InterviewFinal>> timeMap = group.get(date);
+
+            String groupByDay = date.toString();
+            String dayName = date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN);
+
+            List<InterviewTimeTableGroupByTimeDto> groupByTimeList = new ArrayList<>();
+
+            // GroupByDate In GroupByTime 으로 순회
+            for (LocalTime time : getGroupKeyByTime(timeMap)) {
+                List<InterviewFinal> interviewListAtTime = timeMap.get(time);
+                List<TimeTableMemberDetailDto> memberDtoList = new ArrayList<>();
+
+                // M/d, HH:MM에 면접 예정인 면접 정보(InterviewFinal)을 Dto 변환
+                for (InterviewFinal interviewFinal : interviewListAtTime) {
+                    memberDtoList.add(TimeTableMemberDetailDto.from(interviewFinal));
+                }
+
+                groupByTimeList.add(InterviewTimeTableGroupByTimeDto.from(memberDtoList));
+            }
+
+            // 하루 단위 DTO 생성
+            groupByDayList.add(InterviewTimeTableGroupByDayDto
+                    .of(groupByDay, dayName, groupByTimeList));
+        }
+
+        return groupByDayList;
+    }
+
+    private List<LocalDate> getGroupKeyByDate(Map<LocalDate, Map<LocalTime, List<InterviewFinal>>> group) {
+        return group.keySet().stream()
+                .sorted()
+                .toList();
+    }
+
+    private List<LocalTime> getGroupKeyByTime(Map<LocalTime, List<InterviewFinal>> timeMap) {
+        return timeMap.keySet().stream()
+                .sorted()
+                .toList();
+    }
+
 }
