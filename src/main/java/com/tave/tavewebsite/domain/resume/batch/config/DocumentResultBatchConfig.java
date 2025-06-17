02e3f4cfac3a721fb1b0ef25eb283ed21b97aeea
junbeom <jach1206@naver.com>
2025-06-17 11:25:43 +0900

    feat: 서류 평가 완료 배치 Job 및 Step 등록

---
+++ src/main/java/com/tave/tavewebsite/domain/resume/batch/config/DocumentResultBatchConfig.java
@@ -0,0 +1,67 @@
+package com.tave.tavewebsite.domain.resume.batch.config;
+
+import com.tave.tavewebsite.domain.resume.batch.reader.ResumeIdRangePartitioner;
+import com.tave.tavewebsite.domain.resume.entity.Resume;
+import com.tave.tavewebsite.domain.resume.repository.ResumeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class DocumentResultBatchConfig extends DefaultBatchConfiguration {
+
+    private final ItemWriter<Resume> documentResultWriter;
+    private final ResumeRepository resumeRepository;
+    private final JpaPagingItemReader<Resume> resumeReader;
+
+    private final int threadSize = 3;
+
+    @Bean
+    public Job documentResultJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("documentResultJob", jobRepository)
+                .start(partitionedDocumentResultStep(jobRepository, transactionManager))
+                .build();
+    }
+
+    @Bean
+    public Step partitionedDocumentResultStep(JobRepository jobRepository,
+                                              PlatformTransactionManager transactionManager) {
+        return new StepBuilder("partitionedDocumentResultStep", jobRepository)
+                .partitioner("documentResultStep", new ResumeIdRangePartitioner(resumeRepository))
+                .gridSize(threadSize)
+                .step(documentResultStep(jobRepository, transactionManager))
+                .taskExecutor(taskExecutor(threadSize))
+                .build();
+    }
+
+    @Bean
+    public Step documentResultStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("documentResultStep", jobRepository)
+                .<Resume, Resume>chunk(1000, transactionManager)
+                .reader(resumeReader)
+                .writer(documentResultWriter)
+                .build();
+    }
+
+    public TaskExecutor taskExecutor(int threadPoolSize) {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(threadPoolSize);
+        executor.setMaxPoolSize(threadPoolSize);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("document-batch-thread-");
+        executor.initialize();
+        return executor;
+    }
+}
