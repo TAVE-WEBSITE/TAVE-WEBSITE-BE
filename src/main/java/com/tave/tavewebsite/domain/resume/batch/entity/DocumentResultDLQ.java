7724c27acb2e82449e90d7299ac9134dff59c760
junbeom <jach1206@naver.com>
2025-06-17 11:20:33 +0900

    feat: 서류 평가 메일 전용 DLQ 추가

---
+++ src/main/java/com/tave/tavewebsite/domain/resume/batch/entity/DocumentResultDLQ.java
@@ -0,0 +1,47 @@
+package com.tave.tavewebsite.domain.resume.batch.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DocumentResultDLQ {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    @Lob  // TEXT 필드로 저장
+    @Column(columnDefinition = "TEXT")
+    private String errorMessage;
+
+    private LocalDateTime failedAt;
+
+    public static DocumentResultDLQ of(String email, Throwable e) {
+        DocumentResultDLQ dlq = new DocumentResultDLQ();
+        dlq.email = email;
+        dlq.errorMessage = stackTraceToString(e);
+        dlq.failedAt = LocalDateTime.now();
+        return dlq;
+    }
+
+    private static String stackTraceToString(Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
+    }
+}
