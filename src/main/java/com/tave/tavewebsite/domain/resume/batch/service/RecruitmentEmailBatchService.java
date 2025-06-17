b26c3b61a6d7b0002b4e6b2ecf083d8d2e743e61
junbeom <jach1206@naver.com>
2025-06-17 16:14:59 +0900

    feat: 최종 결과 발표 이메일 전송 배치 작업 service 레이어 코드 작성

--- src/main/java/com/tave/tavewebsite/domain/resume/batch/service/RecruitmentEmailBatchService.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/batch/service/RecruitmentEmailBatchService.java
@@ -1,6 +1,7 @@
 package com.tave.tavewebsite.domain.resume.batch.service;
 
 import com.tave.tavewebsite.domain.resume.batch.exception.RecruitmentBatchJobException.DocumentResultBatchJobFailException;
+import com.tave.tavewebsite.domain.resume.batch.exception.RecruitmentBatchJobException.LastResultBatchJobFailException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
@@ -16,12 +17,15 @@ public class RecruitmentEmailBatchService {
 
     private final JobLauncher jobLauncher;
     private final Job documentResultJob;
+    private final Job lastResultJob;
 
     public RecruitmentEmailBatchService(
             JobLauncher jobLauncher,
-            @Qualifier("documentResultJob") Job documentResultJob) {
+            @Qualifier("documentResultJob") Job documentResultJob,
+            @Qualifier("lastResultJob") Job lastResultJob) {
         this.jobLauncher = jobLauncher;
         this.documentResultJob = documentResultJob;
+        this.lastResultJob = lastResultJob;
     }
 
     @Async
@@ -36,6 +40,21 @@ public class RecruitmentEmailBatchService {
         } catch (Exception e) {
             log.error("서류 모집 결과 이메일 발송 처리 실행 실패", e);
             throw new DocumentResultBatchJobFailException();
+        }
+    }
+
+    @Async
+    public void runLastResultJobAsync() {
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("startAt", System.currentTimeMillis())
+                    .toJobParameters();
+            log.info("최종 결과 이메일 발송 배치 처리 시작");
+            jobLauncher.run(lastResultJob, jobParameters);
+            log.info("최종 결과 이메일 발송 배치 처리 완료");
+        } catch (Exception e) {
+            log.error("최종 결과 이메일 발송 처리 실행 실패", e);
+            throw new LastResultBatchJobFailException();
         }
     }
 }
