2f6d7944e4ade45d2fabd3fd33f13f2d0f0e2316
junbeom <jach1206@naver.com>
2025-06-20 13:29:03 +0900

    refactor: 임시 저장 형식에 language levels 필드 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/dto/request/ResumeReqDto.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/dto/request/ResumeReqDto.java
@@ -1,12 +1,13 @@
 package com.tave.tavewebsite.domain.resume.dto.request;
 
+import com.tave.tavewebsite.domain.programinglaunguage.dto.response.LanguageLevelResponseDto;
 import com.tave.tavewebsite.domain.resume.dto.timeslot.TimeSlotReqDto;
-
 import java.util.List;
 
 public record ResumeReqDto(
         List<ResumeAnswerTempDto> answers,
         List<TimeSlotReqDto> timeSlots,
+        List<LanguageLevelResponseDto> languageLevels,
         String githubUrl,
         String blogUrl,
         String portfolioUrl
