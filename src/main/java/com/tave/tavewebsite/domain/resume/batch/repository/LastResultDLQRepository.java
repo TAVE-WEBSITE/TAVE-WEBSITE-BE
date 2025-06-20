e123f8917c6d29bbe766325e1a95dcdc05683780
junbeom <jach1206@naver.com>
2025-06-20 11:13:20 +0900

    refactor: repository 어노테이션 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/batch/repository/LastResultDLQRepository.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/batch/repository/LastResultDLQRepository.java
@@ -2,6 +2,8 @@ package com.tave.tavewebsite.domain.resume.batch.repository;
 
 import com.tave.tavewebsite.domain.resume.batch.entity.LastResultDLQ;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LastResultDLQRepository extends JpaRepository<LastResultDLQ, Long> {
 }
