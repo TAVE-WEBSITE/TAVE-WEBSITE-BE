d6f8dc45b92eb3f52a9000fdb008bebe6112830e
junbeom <jach1206@naver.com>
2025-06-22 20:55:00 +0900

    feat: ProgramingLanguageRequestDto 형식 변경

--- src/main/java/com/tave/tavewebsite/domain/programinglaunguage/dto/request/ProgramingLanguageRequestDto.java
+++ src/main/java/com/tave/tavewebsite/domain/programinglaunguage/dto/request/ProgramingLanguageRequestDto.java
@@ -1,7 +1,9 @@
 package com.tave.tavewebsite.domain.programinglaunguage.dto.request;
 
+import com.tave.tavewebsite.global.common.FieldType;
+
 public record ProgramingLanguageRequestDto(
-        String field,
+        FieldType field,
         String language
 ) {
 }
