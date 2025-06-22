4711bc8c7c1b2150b897e197767b6b66ca1efae8
junbeom <jach1206@naver.com>
2025-06-22 19:58:44 +0900

    feat: 프론트엔드 반환 형식에 맞춰 dto 생성

--- src/main/java/com/tave/tavewebsite/domain/review/dto/request/ReviewRequestDto.java
+++ src/main/java/com/tave/tavewebsite/domain/review/dto/request/ReviewRequestDto.java
@@ -2,10 +2,12 @@ package com.tave.tavewebsite.domain.review.dto.request;
 
 import com.tave.tavewebsite.global.common.FieldType;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record ReviewRequestDto(
 
         String nickname,
+        @Pattern(regexp = "^[0-9]+$", message = "기수는 숫자만 입력 가능합니다.")
         String generation,
         String companyName,
         FieldType field,
