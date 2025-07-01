3c3048880695eb414f3d427166d1611d4dc8b321
koreaioi <koreaioi@gachon.ac.kr>
2025-07-01 13:32:43 +0900

    feat: S3DownloadService - 환경변수 주입 & S3 File Download Method 추가

--- src/main/java/com/tave/tavewebsite/global/s3/service/S3DownloadSerivce.java
+++ src/main/java/com/tave/tavewebsite/global/s3/service/S3DownloadSerivce.java
@@ -2,8 +2,7 @@ package com.tave.tavewebsite.global.s3.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.tave.tavewebsite.domain.interviewfinal.dto.InterviewFormInputStreamDto;
+import com.tave.tavewebsite.domain.interviewfinal.dto.S3ExcelFileInputStreamDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
@@ -19,29 +18,53 @@ public class S3DownloadSerivce {
     private final AmazonS3 s3Client;
     private final String bucketName;
     private final String finalInterviewBasicFormUrl;
+    private final String interviewPossibleTimeTable;
     private static final String FINAL_INTERVIEW_FORM_NAME = "최종 면접 시간표 설정 양식.xlsx";
+    private static final String POSSIBLE_TIME_TABLE_FORM_NAME = "면접자 면접가능시간.xlsx";
     private static final String HEADER_ATTACHMENT = "attachment";
 
-    public S3DownloadSerivce(AmazonS3 s3Client,@Value("${bucket_name}") String bucketName, @Value("${final_interview_form}") String finalInterviewBasicFormUrl) {
+    public S3DownloadSerivce(AmazonS3 s3Client,@Value("${bucket_name}") String bucketName,
+             @Value("${final_interview_form}") String finalInterviewBasicFormUrl,
+             @Value("${interview_possible_time_table}") String interviewPossibleTimeTable
+
+    ) {
         this.s3Client = s3Client;
         this.bucketName = bucketName;
         this.finalInterviewBasicFormUrl = finalInterviewBasicFormUrl;
+        this.interviewPossibleTimeTable = interviewPossibleTimeTable;
     }
 
-    public InterviewFormInputStreamDto downloadInterviewFinalSetUpForm() throws IOException {
+    public S3ExcelFileInputStreamDto downloadInterviewFinalSetUpForm() throws IOException {
+            S3Object s3Object = s3Client.getObject(bucketName, finalInterviewBasicFormUrl);
+            HttpHeaders headers = createHttpHeaders(FINAL_INTERVIEW_FORM_NAME);
 
-        S3Object s3Object = s3Client.getObject(bucketName, finalInterviewBasicFormUrl);
-        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+            return S3ExcelFileInputStreamDto.from(
+                    s3Object.getObjectContent(), headers, s3Object.getObjectMetadata().getContentLength()
+            );
+    }
 
+    public S3ExcelFileInputStreamDto downloadPossibleTimeTableXlsx() throws IOException {
+        S3Object s3Object = s3Client.getObject(bucketName, interviewPossibleTimeTable);
+        HttpHeaders headers = createHttpHeaders(POSSIBLE_TIME_TABLE_FORM_NAME);
 
+        return S3ExcelFileInputStreamDto.from(
+                s3Object.getObjectContent(), headers, s3Object.getObjectMetadata().getContentLength()
+        );
+    }
+
+    /*
+    * refactor
+    * */
+
+    private HttpHeaders createHttpHeaders(String fileName) {
         HttpHeaders headers = new HttpHeaders();
+
         ContentDisposition contentDisposition = ContentDisposition.builder(HEADER_ATTACHMENT)
-                .filename(FINAL_INTERVIEW_FORM_NAME, StandardCharsets.UTF_8)
+                .filename(fileName, StandardCharsets.UTF_8)
                 .build();
 
         headers.setContentDisposition(contentDisposition);
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
-
-        return InterviewFormInputStreamDto.from(inputStream, headers, s3Object.getObjectMetadata().getContentLength());
+        return headers;
     }
 }
