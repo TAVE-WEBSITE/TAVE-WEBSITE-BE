a41198585808f08791ab0f6f2b036fbe0684d6ce
junbeom <jach1206@naver.com>
2025-07-02 11:15:10 +0900

    fix: 사용하지 않는 컨트롤러 제거

--- src/main/java/com/tave/tavewebsite/domain/applicant/history/controller/ApplicantHistoryController.java
+++ src/main/java/com/tave/tavewebsite/domain/applicant/history/controller/ApplicantHistoryController.java
@@ -4,8 +4,6 @@ import com.tave.tavewebsite.domain.applicant.history.dto.response.ApplicantHisto
 import com.tave.tavewebsite.domain.applicant.history.service.ApplicantHistoryService;
 import com.tave.tavewebsite.global.success.SuccessResponse;
 import java.util.List;
-
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,19 +21,5 @@ public class ApplicantHistoryController {
     public SuccessResponse<List<ApplicantHistoryResponseDto>> getApplicantHistory(@PathVariable("id") Long id) {
         return new SuccessResponse<>(applicantHistoryService.getApplicantHistory(id),
                 ApplicantHistorySuccessMessage.READ_SUCCESS.getMessage());
-    }
-
-    @GetMapping("/admin/config/applicant/history/document/status")
-    public SuccessResponse updateApplicantHistoryFromDocument() {
-        applicantHistoryService.changeApplicantStatusFromDocumentStatus();
-        return SuccessResponse.ok(
-                ApplicantHistorySuccessMessage.APPLICANT_STATUS_UPDATE_BY_DOCUMENT_SUCCESS.getMessage());
-    }
-
-    @GetMapping("/admin/config/applicant/history/interview/status")
-    public SuccessResponse updateApplicantHistoryFromInterview(HttpServletRequest request) {
-        applicantHistoryService.changeApplicantStatusFromInterviewStatus(request);
-        return SuccessResponse.ok(
-                ApplicantHistorySuccessMessage.APPLICANT_STATUS_UPDATE_BY_INTERVIEW_SUCCESS.getMessage());
     }
 }
