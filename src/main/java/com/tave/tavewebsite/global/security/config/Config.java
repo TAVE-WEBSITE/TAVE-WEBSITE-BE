b46ff0e3309099f8b9ffbc4cc70e31d5ca9d5601
jiwoo <1114jiwoo@gmail.com>
2025-07-02 14:27:19 +0900

    fix: CORS 오류 해결을 위해 test-recruit 도메인 허용 추가

--- src/main/java/com/tave/tavewebsite/global/security/config/Config.java
+++ src/main/java/com/tave/tavewebsite/global/security/config/Config.java
@@ -41,7 +41,8 @@ public class Config {
                         "https://recruit.tave-wave.com",
                         "https://tave-apply-rjjft5des-taves-projects-79609e27.vercel.app",
                         "https://feat-api.d2hnz1q3fz81jx.amplifyapp.com",
-                        "https://test.tave-wave.com"
+                        "https://test.tave-wave.com",
+                        "https://test-recruit.tave-wave.com"
                 ));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
