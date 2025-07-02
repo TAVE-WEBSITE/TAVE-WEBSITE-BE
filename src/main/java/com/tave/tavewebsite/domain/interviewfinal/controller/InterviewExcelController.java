1893524efe5c29973dfe1c6568a844540f530c7b
koreaioi <koreaioi@gachon.ac.kr>
2025-07-02 14:39:08 +0900

    feat: @RequestMapping 추가 및 권한 Manager로 변경

--- src/main/java/com/tave/tavewebsite/domain/interviewfinal/controller/InterviewExcelController.java
+++ src/main/java/com/tave/tavewebsite/domain/interviewfinal/controller/InterviewExcelController.java
@@ -7,24 +7,26 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
+@RequestMapping("/v1/manager")
 @RequiredArgsConstructor
 public class InterviewExcelController {
 
     private final InterviewExcelUseCase useCase;
 
-    @PostMapping("/v1/normal/excel/interviewer/time-table")
+    @PostMapping("/excel/interviewer/time-table")
     public void createPossibleTimeTableExcel() {
 
         useCase.savePossibleInterviewTimeCSV();
 
     }
 
-    @GetMapping("/v1/normal/excel/interviewer/time-table")
+    @GetMapping("/excel/interviewer/time-table")
     public ResponseEntity<InputStreamResource> downloadPossibleTimeTableExcel() throws IOException {
         S3ExcelFileInputStreamDto dto = useCase.getPossibleInterviewTimeCSV();
 
