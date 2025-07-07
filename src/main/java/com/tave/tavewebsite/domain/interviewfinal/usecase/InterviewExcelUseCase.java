8fe5f2b1efb0f5624a543b5165344798764108dc
koreaioi <koreaioi@gachon.ac.kr>
2025-07-08 05:20:19 +0900

    feat: 면접관 전용 시간표 엑셀 업로드 API 구현

--- src/main/java/com/tave/tavewebsite/domain/interviewfinal/usecase/InterviewExcelUseCase.java
+++ src/main/java/com/tave/tavewebsite/domain/interviewfinal/usecase/InterviewExcelUseCase.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -48,6 +49,10 @@ public class InterviewExcelUseCase {
 
     public S3ExcelFileInputStreamDto getPossibleInterviewTimeCSV() throws IOException {
         return s3DownloadSerivce.downloadPossibleTimeTableXlsx();
+    }
+
+    public void saveInterviewTimeTableForManagerXLSX(MultipartFile file) {
+        s3Service.uploadTimeTableForMangerXLSXToS3(file);
     }
 
     /*
