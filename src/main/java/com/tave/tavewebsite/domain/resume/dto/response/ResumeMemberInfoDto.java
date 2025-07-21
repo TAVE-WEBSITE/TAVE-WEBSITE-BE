b7320bcfb1515423bad81b8fc3a89bfcd1db1f57
koreaioi <koreaioi@gachon.ac.kr>
2025-07-22 02:50:28 +0900

    feat: Dto 재사용을 위해 필드 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/dto/response/ResumeMemberInfoDto.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/dto/response/ResumeMemberInfoDto.java
@@ -10,15 +10,34 @@ public record ResumeMemberInfoDto(
         String sex,
         String email,
         String field,
-        String univ
+        String univ,
+        String birthday,
+        String major,
+        String minor
 ) {
     public static ResumeMemberInfoDto from(Resume resume, Member member) {
         return ResumeMemberInfoDto.builder()
-                .univ(resume.getSchool())
-                .email(member.getEmail())
-                .field(resume.getField().getDisplayName())
                 .username(member.getUsername())
                 .sex(member.getSex().getDisplayName())
+                .email(member.getEmail())
+                .field(resume.getField().getDisplayName())
+                .univ(resume.getSchool())
+                .birthday(member.getBirthday().toString())
+                .major(resume.getMajor())
+                .minor(!resume.isDoubleMajor() ? resume.getMinor() : "")
+                .build();
+    }
+
+    public static ResumeMemberInfoDto of(Resume resume) {
+        return ResumeMemberInfoDto.builder()
+                .username(resume.getMember().getUsername())
+                .sex(resume.getMember().getSex().getDisplayName())
+                .email(resume.getMember().getEmail())
+                .field(resume.getField().getDisplayName())
+                .univ(resume.getSchool())
+                .birthday(resume.getMember().getBirthday().toString())
+                .major(resume.getMajor())
+                .minor(!resume.isDoubleMajor() ? resume.getMinor() : "")
                 .build();
     }
 }
--- src/main/java/com/tave/tavewebsite/domain/resume/dto/response/ResumeResponse.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/dto/response/ResumeResponse.java
@@ -1,15 +1,27 @@
 package com.tave.tavewebsite.domain.resume.dto.response;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record ResumeResponse(
         Long resumeId,
+        ResumeMemberInfoDto resumeMemberInfoDto,
         List<CommonResumeResponse> common,
         List<SpecificResumeResponseDto> specific
 ) {
     public static ResumeResponse of(Long resumeId,
                                     List<CommonResumeResponse> common,
-                                    List<SpecificResumeResponseDto> specific) {
-        return new ResumeResponse(resumeId, common, specific);
+                                    List<SpecificResumeResponseDto> specific,
+                                    ResumeMemberInfoDto resumeMemberInfoDto
+                                    )
+    {
+        return ResumeResponse.builder()
+                .resumeId(resumeId)
+                .resumeMemberInfoDto(resumeMemberInfoDto)
+                .common(common)
+                .specific(specific)
+                .build();
     }
 }
