610b0d9a6114a806ea5168b6247f1b2984c49d15
jiwoo <1114jiwoo@gmail.com>
2025-07-30 17:04:09 +0900

    feat: 포트폴리오 업로드 API 응답 개선 및 DTO 분리

---
+++ src/main/java/com/tave/tavewebsite/domain/resume/dto/response/PortfolioUploadResponseDto.java
@@ -0,0 +1,10 @@
+package com.tave.tavewebsite.domain.resume.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PortfolioUploadResponseDto {
+    private String portfolioUrl;
+}
