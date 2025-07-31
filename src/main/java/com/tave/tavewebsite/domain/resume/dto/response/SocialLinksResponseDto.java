1314f66f14ad3108d238c4b485804dc72237cc4f
jiwoo <1114jiwoo@gmail.com>
2025-08-01 00:39:46 +0900

    feat: 사용자 검증 위해 memberId 추가

--- src/main/java/com/tave/tavewebsite/domain/resume/dto/response/SocialLinksResponseDto.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/dto/response/SocialLinksResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class SocialLinksResponseDto {
+    private Long memberId;
     private String blogUrl;
     private String githubUrl;
     private String portfolioUrl;
