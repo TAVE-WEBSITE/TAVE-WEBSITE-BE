036cea03e24d896049a1c3b4c5d6458b0db49618
wjd4204 <brian876373@gmail.com>
2025-08-03 18:14:44 +0900

    feat: 대시보드 업데이트 API 구현

---
+++ src/main/java/com/tave/tavewebsite/domain/apply/dashboard/repository/DashboardCustomRepository.java
@@ -0,0 +1,8 @@
+package com.tave.tavewebsite.domain.apply.dashboard.repository;
+
+import com.tave.tavewebsite.domain.apply.dashboard.dto.DashboardUpdateDto;
+
+public interface DashboardCustomRepository {
+
+    DashboardUpdateDto countEachElements();
+}
