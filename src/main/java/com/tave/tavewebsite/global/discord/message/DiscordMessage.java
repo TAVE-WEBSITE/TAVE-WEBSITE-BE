eead41d6806dd9267a6bc9ba149466759b45aea7
koreaioi <koreaioi@gachon.ac.kr>
2025-08-07 21:34:04 +0900

    feat: DiscordMessage 추가 - 디스코드 메세지 형식 지정

---
+++ src/main/java/com/tave/tavewebsite/global/discord/message/DiscordMessage.java
@@ -0,0 +1,45 @@
+package com.tave.tavewebsite.global.discord.message;
+
+import com.tave.tavewebsite.domain.member.dto.response.log.MemberLogDto;
+import com.tave.tavewebsite.global.discord.aop.Discordable;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record DiscordMessage(
+        String content,
+        boolean tts,
+        List<DiscordEmbed> embeds
+) {
+    public static DiscordMessage from(MemberLogDto currentMember, Discordable logData) {
+        StringBuilder descriptionBuilder = new StringBuilder();
+
+        if (currentMember != null) {
+            descriptionBuilder.append("🧑 지원자 이름: ").append(currentMember.getUserName()).append("\n");
+            descriptionBuilder.append("📧 지원자 이메일: ").append(currentMember.getEmail()).append("\n");
+        }
+
+        descriptionBuilder.append("🔑 ")
+                .append(logData.getMessageIdentifierKey())
+                .append(": ")
+                .append(logData.getMessageIdentifierValue())
+                .append("\n");
+
+        descriptionBuilder.append("📝 ")
+                .append(logData.getDiscordMessage())
+                .append("\n");
+
+        DiscordEmbed embed = DiscordEmbed.builder()
+                .title("✅ " + logData.getDiscordMessageType().display)
+                .description(descriptionBuilder.toString())
+                .color(5763719)
+                .build();
+
+        return DiscordMessage.builder()
+                .content("📣 새 알림이 도착했습니다!")
+                .tts(false)
+                .embeds(List.of(embed))
+                .build();
+    }
+}
