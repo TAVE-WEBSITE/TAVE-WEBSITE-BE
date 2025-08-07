02a0a6858803e2f8138d0f53d5fb7169737e97ed
koreaioi <koreaioi@gachon.ac.kr>
2025-08-07 21:36:27 +0900

    feat: WebClientConfig 추가 - WebClient 설정

---
+++ src/main/java/com/tave/tavewebsite/global/discord/config/WebClientConfig.java
@@ -0,0 +1,23 @@
+package com.tave.tavewebsite.global.discord.config;
+
+import io.netty.channel.ChannelOption;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient discordWebClient() {
+
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+}
