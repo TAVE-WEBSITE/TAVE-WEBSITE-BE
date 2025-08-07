27ac3b6256cdad3eb49174fac04592843572e80a
koreaioi <koreaioi@gachon.ac.kr>
2025-08-07 21:29:19 +0900

    feat: Discordable 추가 - Discord에 전달하고자하는 행위를 정의

---
+++ src/main/java/com/tave/tavewebsite/global/discord/aop/Discordable.java
@@ -0,0 +1,15 @@
+package com.tave.tavewebsite.global.discord.aop;
+
+/*
+* 전달하고자 하는 Identifier의 Key:Value 전달
+* getDiscordMessage() - Discord에 전달하고자 하는 내용 작성
+* */
+
+public interface Discordable {
+
+    DiscordMessageType getDiscordMessageType();
+    String getMessageIdentifierKey();
+    String getMessageIdentifierValue();
+    String getDiscordMessage();
+
+}
