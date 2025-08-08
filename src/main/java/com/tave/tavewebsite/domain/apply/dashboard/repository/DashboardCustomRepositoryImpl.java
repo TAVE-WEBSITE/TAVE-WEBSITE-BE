70add04f430889fce2da994054dfbafa69f72d28
wjd4204 <brian876373@gmail.com>
2025-08-08 16:44:06 +0900

    refactor: 대시보드에 대해 제출된 이력서만 볼수있게 리팩터링

--- src/main/java/com/tave/tavewebsite/domain/apply/dashboard/repository/DashboardCustomRepositoryImpl.java
+++ src/main/java/com/tave/tavewebsite/domain/apply/dashboard/repository/DashboardCustomRepositoryImpl.java
@@ -10,8 +10,6 @@ import com.tave.tavewebsite.global.common.FieldType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.Objects;
-
 import static com.tave.tavewebsite.domain.member.entity.QMember.member;
 import static com.tave.tavewebsite.domain.resume.entity.QResume.resume;
 
@@ -28,19 +26,21 @@ public class DashboardCustomRepositoryImpl implements DashboardCustomRepository 
                 .select(
                         Projections.constructor(
                                 DashboardUpdateDto.class,
-                                resume.count().coalesce(0L),
+                                resume.id.countDistinct().coalesce(0L),
+
                                 // 성별
-                                new CaseBuilder().when(member.sex.eq(Sex.MALE)).then(1L).otherwise(0L).sum(),
-                                new CaseBuilder().when(member.sex.eq(Sex.FEMALE)).then(1L).otherwise(0L).sum(),
+                                new CaseBuilder().when(member.sex.eq(Sex.MALE)).then(1L).otherwise(0L).sum().coalesce(0L),
+                                new CaseBuilder().when(member.sex.eq(Sex.FEMALE)).then(1L).otherwise(0L).sum().coalesce(0L),
+
                                 // 분야
-                                new CaseBuilder().when(resume.field.eq(FieldType.BACKEND)).then(1L).otherwise(0L).sum(),
-                                new CaseBuilder().when(resume.field.eq(FieldType.WEBFRONTEND)).then(1L).otherwise(0L).sum(),
-                                new CaseBuilder().when(resume.field.eq(FieldType.DESIGN)).then(1L).otherwise(0L).sum(),
-                                new CaseBuilder().when(resume.field.eq(FieldType.APPFRONTEND)).then(1L).otherwise(0L).sum(),
-                                new CaseBuilder().when(resume.field.eq(FieldType.DATAANALYSIS)).then(1L).otherwise(0L).sum(),
-                                new CaseBuilder().when(resume.field.eq(FieldType.DEEPLEARNING)).then(1L).otherwise(0L).sum()
+                                new CaseBuilder().when(resume.field.eq(FieldType.BACKEND)).then(1L).otherwise(0L).sum().coalesce(0L),
+                                new CaseBuilder().when(resume.field.eq(FieldType.WEBFRONTEND)).then(1L).otherwise(0L).sum().coalesce(0L),
+                                new CaseBuilder().when(resume.field.eq(FieldType.DESIGN)).then(1L).otherwise(0L).sum().coalesce(0L),
+                                new CaseBuilder().when(resume.field.eq(FieldType.APPFRONTEND)).then(1L).otherwise(0L).sum().coalesce(0L),
+                                new CaseBuilder().when(resume.field.eq(FieldType.DATAANALYSIS)).then(1L).otherwise(0L).sum().coalesce(0L),
+                                new CaseBuilder().when(resume.field.eq(FieldType.DEEPLEARNING)).then(1L).otherwise(0L).sum().coalesce(0L)
                         )).from(resume)
-                .leftJoin(member)
+                .leftJoin(resume.member, member)
                 .on(
                         member.id.eq(resume.member.id)
                 )
@@ -48,8 +48,8 @@ public class DashboardCustomRepositoryImpl implements DashboardCustomRepository 
                 .fetchOne();
 
         // null 반환 방지: 모두 0으로 채운 DTO
-        return Objects.requireNonNullElseGet(dashboardUpdateDto, () ->
-                new DashboardUpdateDto(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L));
-
+        return (dashboardUpdateDto != null)
+                ? dashboardUpdateDto
+                : new DashboardUpdateDto(0L,0L,0L,0L,0L,0L,0L,0L,0L);
     }
 }
