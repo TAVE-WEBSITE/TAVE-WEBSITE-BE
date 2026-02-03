42f1c8dbc6033629ecc0f28afd8f7e0f43789af4
koreaioi <koreaioi@gachon.ac.kr>
2026-02-03 21:32:38 +0900

    feat: 서류모집기간이 지났는 지 확인하는 API 구현

--- src/main/java/com/tave/tavewebsite/domain/apply/initial/setup/controller/ApplyInitialSetupSuccessMessage.java
+++ src/main/java/com/tave/tavewebsite/domain/apply/initial/setup/controller/ApplyInitialSetupSuccessMessage.java
@@ -10,7 +10,9 @@ public enum ApplyInitialSetupSuccessMessage {
     APPLY_INITIAL_SETUP_SAVE_SUCCESS("지원 초기 설정 저장에 성공했습니다."),
     APPLY_INITIAL_SETUP_UPDATE_SUCCESS("지원 초기 설정 업데이트에 성공했습니다."),
     APPLY_INITIAL_SETUP_DELETE_SUCCESS("지원 초기 설정 삭제에 성공했습니다."),
-    CHECK_APPLY_RECRUIT_END_DATE("지원 기간 여부를 확인합니다.");
+    CHECK_APPLY_RECRUIT_END_DATE("지원 기간 여부를 확인합니다."),
+    CHECK_DOCUMENT_RECRUIT_START_DATE("서류 모집 기간이 시작되었는 지 확인합니다."),
+    ;
 
     private final String message;
 
