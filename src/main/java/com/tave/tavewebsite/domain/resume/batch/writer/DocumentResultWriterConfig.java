455cecfdfd8a71009e4bf4df94c8eb29dd8c1647
koreaioi <koreaioi@gachon.ac.kr>
2026-08-23 13:59:19 +0900

    refactor: 서류합격 V2 메서드 호출로 변경

--- src/main/java/com/tave/tavewebsite/domain/resume/batch/writer/DocumentResultWriterConfig.java
+++ src/main/java/com/tave/tavewebsite/domain/resume/batch/writer/DocumentResultWriterConfig.java
@@ -45,7 +45,10 @@ public class DocumentResultWriterConfig {
             for (Resume item : items) {
                 try {
                     retryTemplate.execute(context -> {
-                        sesMailService.sendDocumentResultMail(item.getMember().getEmail(),
+                        // TODO 수정
+//                        sesMailService.sendDocumentResultMail(item.getMember().getEmail(),
+//                                item.getMember().getUsername(), item.getResumeGeneration());
+                        sesMailService.sendDocumentResultMailV2(item.getFinalDocumentEvaluationStatus(), item.getMember().getEmail(),
                                 item.getMember().getUsername(), item.getResumeGeneration());
                         log.info("메일 전송 성공: {}", item.getMember().getEmail());
                         return null;
